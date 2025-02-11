2eb5f14a301273248c6842c45ee310222c332b05
EnthusedFear <enthceph@gmail.com>
2025-02-11 13:45:45 +0200

    chore

--- packages/config-tailwind/tailwind.config.ts
+++ packages/config-tailwind/tailwind.config.ts
@@ -1,18 +1,13 @@
 import { Config } from "tailwindcss";
 import tailwindAnimate from "tailwindcss-animate";
 
-import tailwindTypography from "@tailwindcss/typography";
-
 import breakpoints from "./extends/breakpoints";
-import colors from "./extends/colors";
 import fontSize from "./extends/fontSize";
 import plugins from "./extends/plugins";
 import typography from "./extends/typography";
 
 const config: Config = {
   theme: {
-    gradientColorStops: colors,
-    colors,
     extend: {
       fontSize: fontSize,
       fontFamily: typography,
@@ -20,7 +15,7 @@ const config: Config = {
     },
   },
   darkMode: ["class", '[data-theme="dark"]'],
-  plugins: [tailwindAnimate, tailwindTypography, ...plugins],
+  plugins: [tailwindAnimate, ...plugins],
 };
 
 export default config;
