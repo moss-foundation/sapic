d817439bf6c668837a5d153a61818532d9a2908c
EnthusedFear <enthceph@gmail.com>
2025-03-04 20:00:31 +0200

    Remove development script reference from index.html and conditionally add it in main.tsx for improved production readiness

--- view/desktop/src/main.tsx
+++ view/desktop/src/main.tsx
@@ -1,12 +1,18 @@
 import React from "react";
-import ReactDOM from "react-dom/client";
+import { createRoot } from "react-dom/client";
 
 import App from "./App";
 
 import "overlayscrollbars/overlayscrollbars.css";
 import "./assets/index.css";
 
-ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
+if (import.meta.env.MODE === "development") {
+  const script = document.createElement("script");
+  script.src = "http://localhost:8097";
+  document.head.appendChild(script);
+}
+
+createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
