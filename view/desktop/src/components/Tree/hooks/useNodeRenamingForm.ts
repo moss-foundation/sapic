de23a8d6cc545353c13758f8e945d79c84746484
EnthusedFear <enthceph@gmail.com>
2025-03-05 22:15:29 +0200

    chore

--- view/desktop/src/components/Tree/hooks/useNodeRenamingForm.ts
+++ view/desktop/src/components/Tree/hooks/useNodeRenamingForm.ts
@@ -3,6 +3,7 @@ import { TreeNodeProps } from "../types";
 
 export const useNodeRenamingForm = (node: TreeNodeProps, onNodeUpdate: (node: TreeNodeProps) => void) => {
     const [isRenamingNode, setIsRenamingNode] = useState(false);
+
     const handleRenamingFormSubmit = (newId: string) => {
         onNodeUpdate({ ...node, id: newId });
         setIsRenamingNode(false);
