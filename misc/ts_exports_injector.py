095569a8cf9c87bf5bc5f4a323597cc1e20a9f65
Hongyu Yang <brutusyhy@gmail.com>
2025-03-09 15:58:07 +0300

    optimize `ts_exports_injector.py`
    Perform only a single file write operation by buffering the output in memory.

--- misc/ts_exports_injector.py
+++ misc/ts_exports_injector.py
@@ -2,8 +2,10 @@ import os
 from pathlib import Path
 
 if __name__ == "__main__":
+    output = []
     with open("index.ts", mode="w") as f:
         for root, dirs, files in os.walk("bindings"):
             for file in files:
                 import_path = "./" + root.replace("\\", "/") + "/" + Path(file).stem
-                f.write(f"export * from \"{import_path}\";\n")
+                output.append(f"export * from \"{import_path}\";")
+        f.write("\n".join(output))
