145314144e2fc04961e13d99216e590b252dcfaf
Hongyu Yang <brutusyhy@gmail.com>
2025-03-11 14:20:47 +0300

    make the output of the `ts_exports_injectory.py` script stable
    `os.walk` by default returns an arbitrary order, which can lead to unnecessary rearranging of lines in the output bindings.

--- misc/ts_exports_injector.py
+++ misc/ts_exports_injector.py
@@ -4,7 +4,7 @@ from pathlib import Path
 if __name__ == "__main__":
     output = []
     with open("index.ts", mode="w") as f:
-        for root, dirs, files in os.walk("bindings"):
+        for root, dirs, files in sorted(os.walk("bindings")):
             for file in files:
                 import_path = "./" + root.replace("\\", "/") + "/" + Path(file).stem
                 output.append(f"export * from \"{import_path}\";")
