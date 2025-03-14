ac8ba963c6cb731281371dab110fde493ef05db7
Hongyu Yang <brutusyhy@gmail.com>
2025-03-14 17:29:08 +0300

    revert some changes

--- crates/moss-collection/src/storage/collection_store.rs
+++ crates/moss-collection/src/storage/collection_store.rs
@@ -1,5 +1,6 @@
 use std::path::PathBuf;
 
+
 use anyhow::Result;
 use moss_db::{bincode_table::BincodeTable, ReDbClient};
 use moss_db::{DatabaseClient, Transaction};
@@ -8,7 +9,8 @@ use crate::models::storage::CollectionEntity;
 
 use super::{CollectionStore, CollectionTable};
 
-const TABLE_COLLECTION: BincodeTable<String, CollectionEntity> = BincodeTable::new("collection");
+const TABLE_COLLECTION: BincodeTable<String, CollectionEntity> =
+    BincodeTable::new("collection");
 
 pub struct CollectionStoreImpl {
     client: ReDbClient,
@@ -19,11 +21,9 @@ impl CollectionStoreImpl {
     pub fn new(client: ReDbClient) -> Self {
         // Initialize by creating the table in the database
         let table = TABLE_COLLECTION;
-        let mut inner_txn = match client.begin_write().unwrap() {
-            Transaction::Read(_) => {
-                unreachable!()
-            }
-            Transaction::Write(txn) => txn,
+        let mut inner_txn= match client.begin_write().unwrap() {
+            Transaction::Read(_) => {unreachable!()}
+            Transaction::Write(txn) => {txn}
         };
         inner_txn.open_table(table.table).unwrap();
         inner_txn.commit().unwrap();
@@ -36,6 +36,7 @@ impl CollectionStoreImpl {
 }
 
 impl CollectionStore for CollectionStoreImpl {
+
     fn begin_write(&self) -> Result<(Transaction, &CollectionTable)> {
         let write_txn = self.client.begin_write()?;
         Ok((write_txn, &self.table))
@@ -48,10 +49,12 @@ impl CollectionStore for CollectionStoreImpl {
 
     fn get_all_items(&self) -> Result<Vec<(PathBuf, CollectionEntity)>> {
         let read_txn = self.client.begin_read()?;
-        Ok(self
-            .table
-            .scan(&read_txn)?
-            .map(|(path, metadata)| (PathBuf::from(path), metadata))
-            .collect())
+        Ok(
+            self.table
+                .scan(&read_txn)?
+                .map(|(path, metadata)| (PathBuf::from(path), metadata))
+                .collect()
+        )
     }
+
 }
