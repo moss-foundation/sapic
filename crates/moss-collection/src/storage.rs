a7fdfa1acdc13b236a5e3cfcbccaeabf64253f6e
Hongyu Yang <brutusyhy@gmail.com>
2025-03-20 10:55:26 +0300

    remove collection_store

--- crates/moss-collection/src/storage.rs
+++ crates/moss-collection/src/storage.rs
@@ -1,5 +1,3 @@
-pub mod collection_store;
-
 pub mod request_store;
 pub mod state_db_manager;
 
@@ -7,17 +5,8 @@ use anyhow::Result;
 use moss_db::{bincode_table::BincodeTable, Transaction};
 use std::{collections::HashMap, path::PathBuf, sync::Arc};
 
-use crate::models::storage::{CollectionEntity, RequestEntity};
+use crate::models::storage::RequestEntity;
 
-pub(crate) type CollectionTable<'a> = BincodeTable<'a, String, CollectionEntity>;
-
-pub trait CollectionStore: Send + Sync + 'static {
-    fn begin_write(&self) -> Result<(Transaction, &CollectionTable)>;
-    fn begin_read(&self) -> Result<(Transaction, &CollectionTable)>;
-    fn get_all_items(&self) -> Result<Vec<(PathBuf, CollectionEntity)>>;
-}
-
-// ------------------------------------------------------------------------------------------------
 
 pub(crate) type RequestStoreTable<'a> = BincodeTable<'a, String, RequestEntity>;
 
