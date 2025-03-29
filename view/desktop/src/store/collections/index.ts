3643ac4a5349bf530531f547520d12046c4b6c17
EnthusedFear <enthceph@gmail.com>
2025-03-29 10:50:04 +0200

    chore(collections): add FIXME comment to indicate placeholder collections

--- view/desktop/src/store/collections/index.ts
+++ view/desktop/src/store/collections/index.ts
@@ -2,6 +2,7 @@ import { create } from "zustand";
 
 import { Collection } from "@/components/Tree/types";
 
+// FIXME: remove when we start using the real collections
 import AzureDevOpsTestCollection from "../../assets/AzureDevOpsTestCollection.json";
 import SapicTestCollection from "../../assets/SapicTestCollection.json";
 import WhatsAppBusinessTestCollection from "../../assets/WhatsAppBusinessTestCollection.json";
