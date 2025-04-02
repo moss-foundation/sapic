e641ea201579cf5cabde9ca837e06772d188798f
EnthusedFear <enthceph@gmail.com>
2025-04-02 17:54:45 +0300

    refactor: Update TreeActionButton to use forwardRef for better ref forwarding and improve button structure

--- view/desktop/src/components/Tree/TreeActionButton.tsx
+++ view/desktop/src/components/Tree/TreeActionButton.tsx
@@ -1,27 +1,27 @@
-import { ComponentPropsWithoutRef } from "react";
+import { ButtonHTMLAttributes, forwardRef } from "react";
 
+import { Icon, Icons } from "@/components";
 import { cn } from "@/utils";
 
-import { Icon, Icons } from "../Icon";
-
-interface TreeActionButtonProps extends ComponentPropsWithoutRef<"button"> {
+interface TreeActionButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
   icon: Icons;
+  className?: string;
 }
 
-export const TreeActionButton = ({ icon, className, ...props }: TreeActionButtonProps) => {
-  return (
-    <div className="flex size-[26px] items-center justify-center">
-      <button
-        className={cn(
-          `background-(--moss-icon-primary-background) hover:background-(--moss-icon-primary-background-hover) disabled:hover:background-transparent disabled:hover:dark:background-transparent flex cursor-pointer items-center justify-center rounded-[3px] p-[3px] text-(--moss-icon-primary-text) disabled:cursor-default disabled:opacity-50 disabled:hover:text-(--moss-icon-primary-text)`,
-          className
-        )}
-        {...props}
-      >
-        <Icon icon={icon} />
+export const TreeActionButton = forwardRef<HTMLButtonElement, TreeActionButtonProps>(
+  ({ icon, className, ...props }, ref) => {
+    return (
+      <button ref={ref} className={cn("flex size-[26px] items-center justify-center", className)} {...props}>
+        <div
+          className={cn(
+            `background-(--moss-icon-primary-background) hover:background-(--moss-icon-primary-background-hover) disabled:hover:background-transparent disabled:hover:dark:background-transparent flex cursor-pointer items-center justify-center rounded-[3px] p-[3px] text-(--moss-icon-primary-text) disabled:cursor-default disabled:opacity-50 disabled:hover:text-(--moss-icon-primary-text)`
+          )}
+        >
+          <Icon icon={icon} />
+        </div>
       </button>
-    </div>
-  );
-};
+    );
+  }
+);
 
 export default TreeActionButton;
