167808314ff714c5395eaa711720b672ff5aa564
brutusyhy <142066452+brutusyhy@users.noreply.github.com>
2025-04-07 08:16:13 +0300

    Make `css_variables_exporter.py` export a json file

--- misc/css_variables_exporter.py
+++ misc/css_variables_exporter.py
@@ -1,13 +1,14 @@
 import cssutils
+import json
 from pathlib import Path
 
 def extract_css_variables(input_file: str, output_file: str):
     """
-    Extracts CSS variables from a CSS file and writes them to a text file.
+    Extracts CSS variables from a CSS file and writes them to a json file.
 
     Args:
         input_file (str): Path to the input CSS file.
-        output_file (str): Path to the output text file where variables will be saved.
+        output_file (str): Path to the output json file where variables will be saved.
     """
     try:
         # Read the CSS file content
@@ -19,13 +20,13 @@ def extract_css_variables(input_file: str, output_file: str):
         # Extract CSS variables from the first rule's style
         variables = [key for key in css.cssRules.item(0).style.keys() if key.startswith('--')]
 
-        # Write the variables to the output file
-        Path(output_file).write_text("\n".join(variables))
+        # Write the variables to the output file as a json array
+        Path(output_file).write_text(json.dumps(variables))
         print(f"CSS variables successfully extracted to {output_file}")
     except Exception as e:
         print(f"An error occurred: {e}")
 
 if __name__ == "__main__":
     input_file = '../assets/themes/light.css'
-    output_file = '../packages/config-eslint/moss-lint-plugin/css_variables.txt'
+    output_file = '../packages/config-eslint/moss-lint-plugin/css_variables.json'
     extract_css_variables(input_file, output_file)
