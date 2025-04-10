886034e26e0e3d2899fc4ed36c6e1b5286ceeb1b
EnthusedFear <enthceph@gmail.com>
2025-04-10 13:34:51 +0300

    added SideBar edge handler

--- view/desktop/src/components/ViewContainer.tsx
+++ view/desktop/src/components/ViewContainer.tsx
@@ -7,7 +7,7 @@ import * as components from "./index";
 export const ViewContainer = ({ groupId }: { groupId: string }) => {
   const { data: viewGroup } = useGetViewGroup(groupId);
 
-  if (!viewGroup) return <div>Loading...</div>;
+  if (!viewGroup) return <div>Empty</div>;
 
   const ComponentToRender = components[viewGroup.component as keyof typeof components] as
     | React.ComponentType<unknown>
--- view/desktop/src/layouts/AppLayout.tsx
+++ view/desktop/src/layouts/AppLayout.tsx
@@ -7,6 +7,7 @@ import "@repo/moss-tabs/assets/styles.css";
 import { testLogEntries } from "@/assets/testLogEntries";
 import { ActivityBar, Scrollbar, Sidebar } from "@/components";
 import { useActivityBarStore } from "@/store/activityBar";
+import { cn } from "@/utils";
 
 import { Resizable, ResizablePanel } from "../components/Resizable";
 import TabbedPane from "../parts/TabbedPane/TabbedPane";
@@ -34,58 +35,72 @@ const DefaultLayout = ({ SideBarPaneContent, MainPaneContent, BottomPaneContent,
   const { position } = useActivityBarStore();
   const { bottomPane, primarySideBar, primarySideBarPosition } = useAppResizableLayoutStore();
 
+  const handleSidebarEdgeHandlerClick = () => {
+    if (!primarySideBar.visible) primarySideBar.setVisible(true);
+  };
+
   return (
     <div className="flex h-full w-full">
       {position === "default" && primarySideBarPosition === "left" && ActivityBar}
-      <Resizable
-        onDragEnd={(sizes) => {
-          if (primarySideBarPosition === "left") {
-            const [leftPanelSize, _mainPanelSize] = sizes;
-            primarySideBar.setWidth(leftPanelSize);
-          }
-          if (primarySideBarPosition === "right") {
-            const [_mainPanelSize, rightPanelSize] = sizes;
-            primarySideBar.setWidth(rightPanelSize);
-          }
-        }}
-        onVisibleChange={(index, visible) => {
-          if (primarySideBarPosition === "left" && index === 0) primarySideBar.setVisible(visible);
-          if (primarySideBarPosition === "right" && index === 1) primarySideBar.setVisible(visible);
-        }}
-      >
-        {primarySideBarPosition === "left" && (
-          <ResizablePanel
-            preferredSize={primarySideBar.width}
-            visible={primarySideBar.visible && primarySideBarPosition === "left"}
-            minSize={primarySideBar.minWidth}
-            snap
-            className="background-(--moss-primary-background)"
-          >
-            {SideBarPaneContent}
-          </ResizablePanel>
+      <div className="relative flex h-full w-full">
+        {!primarySideBar.visible && primarySideBarPosition === "left" && (
+          <SidebarEdgeHandler alignment="left" onClick={handleSidebarEdgeHandlerClick} />
         )}
 
-        <ResizablePanel>
-          <Resizable vertical>
-            <ResizablePanel>{MainPaneContent}</ResizablePanel>
-            <ResizablePanel visible={bottomPane.visible} minSize={bottomPane.minHeight} snap>
-              {BottomPaneContent}
+        <Resizable
+          className=""
+          onDragEnd={(sizes) => {
+            if (primarySideBarPosition === "left") {
+              const [leftPanelSize, _mainPanelSize] = sizes;
+              primarySideBar.setWidth(leftPanelSize);
+            }
+            if (primarySideBarPosition === "right") {
+              const [_mainPanelSize, rightPanelSize] = sizes;
+              primarySideBar.setWidth(rightPanelSize);
+            }
+          }}
+          onVisibleChange={(index, visible) => {
+            if (primarySideBarPosition === "left" && index === 0) primarySideBar.setVisible(visible);
+            if (primarySideBarPosition === "right" && index === 1) primarySideBar.setVisible(visible);
+          }}
+        >
+          {primarySideBarPosition === "left" && (
+            <ResizablePanel
+              preferredSize={primarySideBar.width}
+              visible={primarySideBar.visible && primarySideBarPosition === "left"}
+              minSize={primarySideBar.minWidth}
+              snap
+              className="background-(--moss-primary-background)"
+            >
+              {SideBarPaneContent}
             </ResizablePanel>
-          </Resizable>
-        </ResizablePanel>
-
-        {primarySideBarPosition === "right" && (
-          <ResizablePanel
-            preferredSize={primarySideBar.width}
-            visible={primarySideBar.visible && primarySideBarPosition === "right"}
-            minSize={primarySideBar.minWidth}
-            snap
-            className="background-(--moss-primary-background)"
-          >
-            {SideBarPaneContent}
+          )}
+          <ResizablePanel>
+            <Resizable vertical>
+              <ResizablePanel>{MainPaneContent}</ResizablePanel>
+              <ResizablePanel visible={bottomPane.visible} minSize={bottomPane.minHeight} snap>
+                {BottomPaneContent}
+              </ResizablePanel>
+            </Resizable>
           </ResizablePanel>
+
+          {primarySideBarPosition === "right" && (
+            <ResizablePanel
+              preferredSize={primarySideBar.width}
+              visible={primarySideBar.visible && primarySideBarPosition === "right"}
+              minSize={primarySideBar.minWidth}
+              snap
+              className="background-(--moss-primary-background)"
+            >
+              {SideBarPaneContent}
+            </ResizablePanel>
+          )}
+        </Resizable>
+
+        {!primarySideBar.visible && primarySideBarPosition === "right" && (
+          <SidebarEdgeHandler alignment="right" onClick={handleSidebarEdgeHandlerClick} />
         )}
-      </Resizable>
+      </div>
 
       {position === "default" && primarySideBarPosition === "right" && ActivityBar}
     </div>
@@ -136,6 +151,66 @@ const BottomPaneContent = () => {
           ))}
         </div>
       </Scrollbar>
+    </div>
+  );
+};
+
+interface SidebarEdgeHandlerProps {
+  alignment?: "left" | "right";
+  onClick?: () => void;
+}
+
+const SidebarEdgeHandler = ({ alignment, onClick }: SidebarEdgeHandlerProps) => {
+  return (
+    <div
+      className={cn("group/openHandle absolute z-40 h-full w-2 cursor-pointer", {
+        "left-0": alignment === "left",
+        "right-0": alignment === "right",
+      })}
+      onClick={onClick}
+    >
+      <div
+        className={cn(
+          "background-(--moss-info-background-hover)/70 absolute top-0 z-40 h-full w-3 opacity-0 transition-[opacity] duration-100 group-hover/openHandle:opacity-100",
+          {
+            "left-0": alignment === "left",
+            "right-0": alignment === "right",
+          }
+        )}
+      />
+
+      <div
+        className={cn(
+          "background-(--moss-info-icon)/50 group-hover/openHandle:background-(--moss-info-icon)/80 absolute inset-y-[calc(50%-64px)] z-40 h-32 w-1.5 rounded transition-[opacity,translate] duration-100",
+          {
+            "left-[3px]": alignment === "left",
+            "right-[3px]": alignment === "right",
+          }
+        )}
+      />
+
+      <div
+        className={cn(
+          "background-(--moss-info-icon) absolute inset-y-[calc(50%-12px)] z-40 flex size-6 items-center justify-center rounded-full opacity-0 transition-[opacity,translate] duration-100 group-hover/openHandle:opacity-100",
+          {
+            "left-1 group-hover/openHandle:translate-x-0.5": alignment === "left",
+            "right-1 group-hover/openHandle:-translate-x-0.5": alignment === "right",
+          }
+        )}
+      >
+        <svg
+          width="16"
+          height="16"
+          viewBox="0 0 16 16"
+          fill="none"
+          xmlns="http://www.w3.org/2000/svg"
+          className={cn({
+            "rotate-180": alignment === "right",
+          })}
+        >
+          <path d="M6 3L11 8L6 13" stroke="white" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round" />
+        </svg>
+      </div>
     </div>
   );
 };
