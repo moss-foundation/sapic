9f7df622a3b96718c47027cda235fed38d8878e9
Enthceph <enthceph@gmail.com>
2025-04-12 15:11:02 +0300

    removed hardcoded styles in LinuxControls

--- view/desktop/src/parts/HeadBar/Controls/LinuxControls.tsx
+++ view/desktop/src/parts/HeadBar/Controls/LinuxControls.tsx
@@ -15,13 +15,13 @@ export function LinuxControls({ className, ...props }: HTMLProps<HTMLDivElement>
       <CommonControls />
       <ControlButton
         onClick={minimizeWindow}
-        className="background-(--moss-windowControlsLinux-background) hover:background-(--moss-windowControlsLinux-hoverBackground) active:background-(--moss-windowControlsLinux-activeBackground) size-6 cursor-default rounded-full text-(--moss-windowControlsLinux-text) dark:bg-[#373737] dark:text-white dark:hover:bg-[#424242] dark:active:bg-[#565656]"
+        className="background-(--moss-windowControlsLinux-background) hover:background-(--moss-windowControlsLinux-hoverBackground) active:background-(--moss-windowControlsLinux-activeBackground) size-6 cursor-default rounded-full text-(--moss-windowControlsLinux-text) dark:text-white dark:hover:bg-[#424242] dark:active:bg-[#565656]"
       >
         <ControlsIcons.minimizeWin className="size-[9px]" />
       </ControlButton>
       <ControlButton
         onClick={maximizeWindow}
-        className="background-(--moss-windowControlsLinux-background) hover:background-(--moss-windowControlsLinux-hoverBackground) active:background-(--moss-windowControlsLinux-activeBackground) dark:background-[#373737] size-6 cursor-default rounded-full text-(--moss-windowControlsLinux-text) dark:text-white dark:hover:bg-[#424242] dark:active:bg-[#565656]"
+        className="background-(--moss-windowControlsLinux-background) hover:background-(--moss-windowControlsLinux-hoverBackground) active:background-(--moss-windowControlsLinux-activeBackground) size-6 cursor-default rounded-full text-(--moss-windowControlsLinux-text) dark:text-white dark:hover:bg-[#424242] dark:active:bg-[#565656]"
       >
         {isWindowMaximized ? (
           <ControlsIcons.maximizeRestoreWin className="size-[9px]" />
@@ -31,7 +31,7 @@ export function LinuxControls({ className, ...props }: HTMLProps<HTMLDivElement>
       </ControlButton>
       <ControlButton
         onClick={closeWindow}
-        className="background-(--moss-windowControlsLinux-background) hover:background-(--moss-windowControlsLinux-hoverBackground) active:background-(--moss-windowControlsLinux-activeBackground) size-6 cursor-default rounded-full text-(--moss-windowControlsLinux-text) dark:bg-[#373737] dark:text-white dark:hover:bg-[#424242] dark:active:bg-[#565656]"
+        className="background-(--moss-windowControlsLinux-background) hover:background-(--moss-windowControlsLinux-hoverBackground) active:background-(--moss-windowControlsLinux-activeBackground) size-6 cursor-default rounded-full text-(--moss-windowControlsLinux-text) dark:text-white dark:hover:bg-[#424242] dark:active:bg-[#565656]"
       >
         <ControlsIcons.closeWin className="size-2" />
       </ControlButton>
