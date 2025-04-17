411e4a7c2f5bb34a0aff804098ab14662950acbf
EnthusedFear <enthceph@gmail.com>
2025-04-17 13:44:10 +0300

    fixed Dockview layout wouldn't save on some events

--- view/desktop/src/parts/TabbedPane/hooks/useDockviewEventHandlers.ts
+++ view/desktop/src/parts/TabbedPane/hooks/useDockviewEventHandlers.ts
@@ -17,53 +17,42 @@ export const useDockviewEventHandlers = (
     if (!api) return;
 
     const disposables = [
-      api.onDidLayoutChange(() => {
-        console.log("onDidLayoutChange");
-        setGridState(api.toJSON());
-      }),
       api.onDidAddPanel((event) => {
-        console.log("onDidAddPanel");
         setPanels((prev) => [...prev, event.id]);
         addLogLine(`Panel Added ${event.id}`);
-        setGridState(api.toJSON());
       }),
       api.onDidActivePanelChange((event) => {
-        console.log("onDidActivePanelChange");
         setActivePanel(event?.id);
         setActivePanelId(event?.id);
         addLogLine(`Panel Activated ${event?.id}`);
       }),
       api.onDidRemovePanel((event) => {
-        console.log("onDidRemovePanel");
         setPanels((prev) => prev.filter((id) => id !== event.id));
         addLogLine(`Panel Removed ${event?.id || "unknown"}`);
       }),
       api.onDidAddGroup((event) => {
-        console.log("onDidAddGroup");
         setGroups((prev) => [...prev, event.id]);
         addLogLine(`Group Added ${event.id}`);
       }),
       api.onDidMovePanel((event) => {
-        console.log("onDidMovePanel");
         addLogLine(`Panel Moved ${event.panel.id}`);
       }),
       api.onDidMaximizedGroupChange((event) => {
-        console.log("onDidMaximizedGroupChange");
         addLogLine(`Group Maximized Changed ${event.group.api.id} [${event.isMaximized}]`);
       }),
       api.onDidRemoveGroup((event) => {
-        console.log("onDidRemoveGroup");
         setGroups((prev) => prev.filter((id) => id !== event.id));
         addLogLine(`Group Removed ${event.id}`);
       }),
       api.onDidActiveGroupChange((event) => {
-        console.log("onDidActiveGroupChange");
         setActiveGroup(event?.id);
         addLogLine(`Group Activated ${event?.id}`);
       }),
       api.onUnhandledDragOverEvent((event) => {
-        console.log("onUnhandledDragOverEvent");
         event.accept();
+      }),
+      api.onDidLayoutChange(() => {
+        setGridState(api.toJSON());
       }),
     ];
 
