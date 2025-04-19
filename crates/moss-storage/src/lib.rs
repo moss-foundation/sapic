e28348022bfd7f00a157b18103f67e07718e1e2c
g10z3r <20g10z3r@gmail.com>
2025-04-19 12:42:17 +0200

    add collection_storage module and define traits for storage management

--- crates/moss-storage/src/lib.rs
+++ crates/moss-storage/src/lib.rs
@@ -1,3 +1,4 @@
+pub mod collection_storage;
 pub mod global_storage;
 pub mod workspace_storage;
 
@@ -8,3 +9,7 @@ use global_storage::WorkspacesStore;
 pub trait GlobalStorage: Send + Sync {
     fn workspaces_store(&self) -> Arc<dyn WorkspacesStore>;
 }
+
+pub trait WorkspaceStorage: Send + Sync {}
+
+pub trait CollectionStorage: Send + Sync {}
--- crates/moss-storage/src/workspace_storage.rs
+++ crates/moss-storage/src/workspace_storage.rs
@@ -1,17 +1,5 @@
-use std::path::PathBuf;
-
-use moss_db::{common::DatabaseError, ReDbClient};
+pub mod entities;
 
 const WORKSPACE_STATE_DB_NAME: &str = "state.db";
 
 pub struct WorkspaceStorageImpl {}
-
-// impl WorkspaceStorageImpl {
-//     pub fn new(workspace_path: &PathBuf) -> Result<Self, DatabaseError> {
-//         let db_client = ReDbClient::new(workspace_path.join(WORKSPACE_STATE_DB_NAME))?
-//             .with_table(&TABLE_COLLECTIONS)?
-//             .with_table(&TABLE_PARTS_STATE)?;
-
-//         Ok(Self {})
-//     }
-// }
