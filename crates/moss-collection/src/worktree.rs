b575f54b475830edb175ec2aa141756cd7ca6091
g10z3r <20g10z3r@gmail.com>
2025-04-22 15:40:21 +0200

    basic fields

--- crates/moss-collection/src/worktree.rs
+++ crates/moss-collection/src/worktree.rs
@@ -1,14 +1,36 @@
-use std::sync::{atomic::AtomicUsize, Arc};
+use std::{
+    path::PathBuf,
+    sync::{atomic::AtomicUsize, Arc},
+    time::SystemTime,
+};
 
+use sweep_bptree::BPlusTreeMap;
 use tokio::sync::watch;
+
+pub struct Scanner {}
+
+pub enum EntryKind {}
+
+pub struct Entry {
+    id: usize,
+    kind: EntryKind,
+    mtime: Option<SystemTime>,
+    size: u64,
+}
 
 pub struct Snapshot {
     id: Arc<AtomicUsize>,
-    // entries_by_path: todo!(),
+    entries_by_path: BPlusTreeMap<PathBuf, Entry>,
     // entries_by_id: todo!(),
 }
 
 pub struct Worktree {
     next_snapshot_id: Arc<AtomicUsize>,
     is_scanning: (watch::Sender<bool>, watch::Receiver<bool>),
+}
+
+impl Worktree {
+    pub fn new() -> Self {
+        todo!()
+    }
 }
