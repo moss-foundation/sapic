70220ed45dee2fc74f137d56bb3c39b136a88a3c
g10z3r <20g10z3r@gmail.com>
2025-04-22 18:52:18 +0200

    add file watching functionality to FileSystem trait

--- crates/moss-collection/src/worktree.rs
+++ crates/moss-collection/src/worktree.rs
@@ -1,36 +1,101 @@
+use std::sync::atomic::Ordering::SeqCst;
 use std::{
     path::PathBuf,
     sync::{atomic::AtomicUsize, Arc},
     time::SystemTime,
 };
-
 use sweep_bptree::BPlusTreeMap;
-use tokio::sync::watch;
+use tokio::sync::Barrier;
+use tokio::sync::{mpsc::UnboundedReceiver, watch, Mutex};
+use tokio::task::JoinHandle;
 
-pub struct Scanner {}
+struct ScanRequest {
+    relative_paths: Vec<PathBuf>,
+    done: Barrier,
+}
 
-pub enum EntryKind {}
+pub enum BackgroundScannerPhase {
+    InitialScan,
+}
+
+pub struct BackgroundScannerState {}
+
+pub struct BackgroundScanner {
+    state: Mutex<BackgroundScannerState>,
+    next_entry_id: Arc<AtomicUsize>,
+    phase: BackgroundScannerPhase,
+    scan_req_rx: UnboundedReceiver<ScanRequest>,
+}
+
+pub enum UnitType {
+    Request,
+    Endpoint,
+    Schema,
+    Component,
+}
+
+#[derive(Clone, Copy, Debug, PartialEq, Eq, PartialOrd, Ord)]
+pub struct EntryId(usize);
+
+impl EntryId {
+    pub fn new(counter: &AtomicUsize) -> Self {
+        Self(counter.fetch_add(1, SeqCst))
+    }
+
+    pub fn to_usize(&self) -> usize {
+        self.0
+    }
+}
+
+pub enum EntryKind {
+    Unit,
+    Folder,
+}
 
 pub struct Entry {
-    id: usize,
+    id: EntryId,
     kind: EntryKind,
+    unit_type: Option<UnitType>,
     mtime: Option<SystemTime>,
     size: u64,
 }
 
 pub struct Snapshot {
-    id: Arc<AtomicUsize>,
-    entries_by_path: BPlusTreeMap<PathBuf, Entry>,
-    // entries_by_id: todo!(),
+    scan_id: usize,
+    entries_by_id: BPlusTreeMap<EntryId, Entry>,
+    entries_by_path: BPlusTreeMap<PathBuf, EntryId>,
 }
 
 pub struct Worktree {
-    next_snapshot_id: Arc<AtomicUsize>,
+    snapshot: Snapshot,
     is_scanning: (watch::Sender<bool>, watch::Receiver<bool>),
+    background_tasks: Vec<JoinHandle<()>>,
 }
 
 impl Worktree {
     pub fn new() -> Self {
-        todo!()
+        let snapshot = Snapshot {
+            scan_id: 0,
+            entries_by_id: BPlusTreeMap::new(),
+            entries_by_path: BPlusTreeMap::new(),
+        };
+
+        let scanner_handle: JoinHandle<()> = tokio::spawn(async move {
+            // loop {
+            //     tokio::select! {
+
+            //     }
+            // }
+        });
+
+        let updater_handle: JoinHandle<()> = tokio::spawn(async move {
+            //
+        });
+
+        Self {
+            snapshot,
+            is_scanning: watch::channel(true),
+            background_tasks: vec![scanner_handle, updater_handle],
+        }
     }
 }
--- crates/moss-fs/src/lib.rs
+++ crates/moss-fs/src/lib.rs
@@ -4,7 +4,12 @@ pub mod utils;
 pub use real::*;
 
 use anyhow::Result;
-use std::{io, path::Path};
+use futures::stream::BoxStream;
+use std::{
+    io,
+    path::{Path, PathBuf},
+    time::Duration,
+};
 use tokio::fs::ReadDir;
 
 // TODO: Rename to RemoveParams
@@ -63,4 +68,12 @@ pub trait FileSystem: Send + Sync {
     ) -> Result<()>;
     async fn remove_file(&self, path: &Path, options: RemoveOptions) -> Result<()>;
     async fn open_file(&self, path: &Path) -> Result<Box<dyn io::Read + Send + Sync>>;
+    fn watch(
+        &self,
+        path: PathBuf,
+        latency: Duration,
+    ) -> Result<(
+        BoxStream<'static, Vec<notify::Event>>,
+        notify::RecommendedWatcher,
+    )>;
 }
