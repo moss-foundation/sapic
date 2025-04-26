121397b3f45e0d04d062a8200c7c0957b4640030
EnthusedFear <enthceph@gmail.com>
2025-04-26 15:01:20 +0300

    refactor: update PlusButton SVG for improved accessibility and adjust padding in CollectionTreeView component

--- view/desktop/src/components/CollectionTreeView.tsx
+++ view/desktop/src/components/CollectionTreeView.tsx
@@ -105,7 +105,7 @@ export const CollectionTreeView = () => {
     <div ref={dropTargetToggleRef} className="relative h-[calc(100%-36px)] select-none">
       <Scrollbar className="h-full">
         <div className="flex h-full flex-col">
-          <div className="flex shrink items-center gap-[7px] px-2 py-1">
+          <div className="flex shrink items-center gap-[7px] py-1 pr-2.5 pl-2">
             <Input
               variant="plain"
               onInput={(e) => setSearchInput((e.target as HTMLInputElement).value)}
