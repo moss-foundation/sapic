1c8bf67ae5ac85f8c8f24a2ee8d5176be5b88f49
Enthceph <enthceph@gmail.com>
2025-04-28 14:55:12 +0300

    fix light theme for linux window controls

--- view/desktop/src/parts/HeadBar/Controls/LinuxControls.tsx
+++ view/desktop/src/parts/HeadBar/Controls/LinuxControls.tsx
@@ -1,3 +1,4 @@
+import { cva } from "class-variance-authority";
 import { useContext, type HTMLProps } from "react";
 
 import { cn } from "@/utils";
@@ -7,32 +8,31 @@ import { ControlButton } from "./ControlButton";
 import ControlsContext from "./ControlsContext";
 import { ControlsIcons } from "./icons";
 
+const linuxControlButtonStyles = cva(`
+  size-6 cursor-default rounded-full
+  background-(--moss-windowControlsLinux-background) 
+  hover:background-(--moss-windowControlsLinux-hoverBackground) 
+  active:background-(--moss-windowControlsLinux-activeBackground)  
+  text-(--moss-windowControlsLinux-text)
+`);
+
 export function LinuxControls({ className, ...props }: HTMLProps<HTMLDivElement>) {
   const { isWindowMaximized, minimizeWindow, maximizeWindow, closeWindow } = useContext(ControlsContext);
 
   return (
-    <div className={cn("mr-2.5 flex h-auto items-center space-x-[13px]", className)} {...props}>
+    <div className={cn("mr-2.5 flex h-auto items-center gap-3.25", className)} {...props}>
       <CommonControls />
-      <ControlButton
-        onClick={minimizeWindow}
-        className="background-(--moss-windowControlsLinux-background) hover:background-(--moss-windowControlsLinux-hoverBackground) active:background-(--moss-windowControlsLinux-activeBackground) size-6 cursor-default rounded-full text-(--moss-windowControlsLinux-text) dark:text-white dark:hover:bg-[#424242] dark:active:bg-[#565656]"
-      >
+      <ControlButton onClick={minimizeWindow} className={linuxControlButtonStyles()}>
         <ControlsIcons.minimizeWin className="size-[9px]" />
       </ControlButton>
-      <ControlButton
-        onClick={maximizeWindow}
-        className="background-(--moss-windowControlsLinux-background) hover:background-(--moss-windowControlsLinux-hoverBackground) active:background-(--moss-windowControlsLinux-activeBackground) size-6 cursor-default rounded-full text-(--moss-windowControlsLinux-text) dark:text-white dark:hover:bg-[#424242] dark:active:bg-[#565656]"
-      >
+      <ControlButton onClick={maximizeWindow} className={linuxControlButtonStyles()}>
         {isWindowMaximized ? (
           <ControlsIcons.maximizeRestoreWin className="size-[9px]" />
         ) : (
           <ControlsIcons.maximizeWin className="size-2" />
         )}
       </ControlButton>
-      <ControlButton
-        onClick={closeWindow}
-        className="background-(--moss-windowControlsLinux-background) hover:background-(--moss-windowControlsLinux-hoverBackground) active:background-(--moss-windowControlsLinux-activeBackground) size-6 cursor-default rounded-full text-(--moss-windowControlsLinux-text) dark:text-white dark:hover:bg-[#424242] dark:active:bg-[#565656]"
-      >
+      <ControlButton onClick={closeWindow} className={linuxControlButtonStyles()}>
         <ControlsIcons.closeWin className="size-2" />
       </ControlButton>
     </div>
