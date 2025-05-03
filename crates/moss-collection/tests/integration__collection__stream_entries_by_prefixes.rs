08eb819df7be51dd8ae07980256d4772ba7455ac
g10z3r <20g10z3r@gmail.com>
2025-05-03 17:25:57 +0200

    remove unused code and comment out test function for future implementation

--- crates/moss-collection/tests/integration__collection__stream_entries_by_prefixes.rs
+++ crates/moss-collection/tests/integration__collection__stream_entries_by_prefixes.rs
@@ -1,56 +1,40 @@
 mod shared;
 
-use std::time::Duration;
-
-use moss_collection::{
-    collection::Collection,
-    models::operations::{EntryInfo, StreamEntriesByPrefixesEvent, StreamEntriesByPrefixesInput},
+use moss_collection::models::operations::{
+    StreamEntriesByPrefixesEvent, StreamEntriesByPrefixesInput,
 };
-use tokio::sync::mpsc;
 
 use crate::shared::set_up_test_collection;
 
-#[derive(Clone)]
-struct TestChannel {
-    tx: mpsc::UnboundedSender<StreamEntriesByPrefixesEvent>,
-}
-impl TestChannel {
-    fn new(tx: mpsc::UnboundedSender<StreamEntriesByPrefixesEvent>) -> Self {
-        Self { tx }
-    }
-    pub fn send(&self, event: StreamEntriesByPrefixesEvent) -> Result<(), String> {
-        self.tx.send(event).map_err(|e| e.to_string())
-    }
-}
+// TODO:
+// #[tokio::test]
+// async fn test() {
+//     let (collection_path, collection) = set_up_test_collection().await;
+//     // let (evt_tx, mut evt_rx) = mpsc::unbounded_channel::<ListEntriesEvent>();
+//     // let on_event = TestChannel::new(evt_tx.clone());
+//     // let on_event: tauri::ipc::Channel<ListEntriesEvent> = unsafe { std::mem::transmute(on_event) };
 
-#[tokio::test]
-async fn test() {
-    let (collection_path, collection) = set_up_test_collection().await;
-    // let (evt_tx, mut evt_rx) = mpsc::unbounded_channel::<ListEntriesEvent>();
-    // let on_event = TestChannel::new(evt_tx.clone());
-    // let on_event: tauri::ipc::Channel<ListEntriesEvent> = unsafe { std::mem::transmute(on_event) };
+//     let variants = vec!["endpoints", "components", "schemas", "cases", "requests"];
 
-    let variants = vec!["endpoints", "components", "schemas", "cases", "requests"];
+//     let on_event = tauri::ipc::Channel::<StreamEntriesByPrefixesEvent>::new(|event| {
+//         dbg!(&event);
 
-    let on_event = tauri::ipc::Channel::<StreamEntriesByPrefixesEvent>::new(|event| {
-        dbg!(&event);
+//         Ok(())
+//     });
 
-        Ok(())
-    });
+//     let _ = collection
+//         .stream_entries_by_prefixes(on_event, StreamEntriesByPrefixesInput(variants))
+//         .await;
 
-    let _ = collection
-        .stream_entries_by_prefixes(on_event, StreamEntriesByPrefixesInput(variants))
-        .await;
+//     // let mut all: Vec<EntryInfo> = Vec::new();
 
-    // let mut all: Vec<EntryInfo> = Vec::new();
+//     // while let Some(ListEntriesEvent(batch)) = evt_rx.recv().await {
+//     //     dbg!(&batch);
 
-    // while let Some(ListEntriesEvent(batch)) = evt_rx.recv().await {
-    //     dbg!(&batch);
+//     //     if batch.is_empty() {
+//     //         break;
+//     //     }
 
-    //     if batch.is_empty() {
-    //         break;
-    //     }
-
-    //     all.extend(batch);
-    // }
-}
+//     //     all.extend(batch);
+//     // }
+// }
