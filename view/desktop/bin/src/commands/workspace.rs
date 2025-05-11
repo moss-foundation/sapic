3b74a6a2d7646e4ca33e37268f7ad2594b02f644
g10z3r <20g10z3r@gmail.com>
2025-05-11 12:27:29 +0200

    Refactor command structure and introduce app and workbench modules
    - Removed the `cmd_base` module and integrated its functionality into the new `app` and `workbench` modules.
    - Added new commands for managing color themes, locales, and workspace operations in the `app` and `workbench` modules.
    - Updated the `commands.rs` file to reflect the new module structure and ensure proper exports.
    - Enhanced workspace management commands to improve functionality and error handling.

--- view/desktop/bin/src/commands/workspace.rs
+++ view/desktop/bin/src/commands/workspace.rs
@@ -1,85 +1,8 @@
 use moss_app::manager::AppManager;
 use moss_tauri::{TauriError, TauriResult};
-use moss_workbench::{models::operations::*, workbench::Workbench};
+use moss_workbench::workbench::Workbench;
 use moss_workspace::models::operations::{DescribeStateOutput, UpdateStateInput};
 use tauri::{Runtime as TauriRuntime, State, Window};
-
-#[tauri::command(async)]
-#[instrument(level = "trace", skip(app_manager), fields(window = window.label()))]
-pub async fn create_workspace<R: TauriRuntime>(
-    app_manager: State<'_, AppManager<R>>,
-    window: Window<R>,
-    input: CreateWorkspaceInput,
-) -> TauriResult<CreateWorkspaceOutput> {
-    let app_handle = app_manager.app_handle();
-    let workspace_manager = app_manager
-        .services()
-        .get_by_type::<Workbench<R>>(&app_handle)
-        .await?;
-
-    let workspace_output = workspace_manager
-        .create_workspace(&input)
-        .await
-        .map_err(|err| TauriError(format!("Failed to create workspace: {}", err)))?; // TODO: improve error handling
-
-    Ok(workspace_output)
-}
-
-#[tauri::command(async)]
-#[instrument(level = "trace", skip(app_manager), fields(window = window.label()))]
-pub async fn list_workspaces<R: TauriRuntime>(
-    app_manager: State<'_, AppManager<R>>,
-    window: Window<R>,
-) -> TauriResult<ListWorkspacesOutput> {
-    let app_handle = app_manager.app_handle();
-    let workspace_manager = app_manager
-        .services()
-        .get_by_type::<Workbench<R>>(&app_handle)
-        .await?;
-
-    workspace_manager
-        .list_workspaces()
-        .await
-        .map_err(|err| TauriError(format!("Failed to list workspaces: {}", err))) // TODO: improve error handling
-}
-
-#[tauri::command(async)]
-#[instrument(level = "trace", skip(app_manager), fields(window = window.label()))]
-pub async fn delete_workspace<R: TauriRuntime>(
-    app_manager: State<'_, AppManager<R>>,
-    window: Window<R>,
-    input: DeleteWorkspaceInput,
-) -> TauriResult<()> {
-    let app_handle = app_manager.app_handle();
-    let workspace_manager = app_manager
-        .services()
-        .get_by_type::<Workbench<R>>(&app_handle)
-        .await?;
-
-    workspace_manager
-        .delete_workspace(&input)
-        .await
-        .map_err(|err| TauriError(format!("Failed to delete workspace: {}", err))) // TODO: improve error handling
-}
-
-#[tauri::command(async)]
-#[instrument(level = "trace", skip(app_manager), fields(window = window.label()))]
-pub async fn open_workspace<R: TauriRuntime>(
-    app_manager: State<'_, AppManager<R>>,
-    window: Window<R>,
-    input: OpenWorkspaceInput,
-) -> TauriResult<OpenWorkspaceOutput> {
-    let app_handle = app_manager.app_handle();
-    let workspace_manager = app_manager
-        .services()
-        .get_by_type::<Workbench<R>>(&app_handle)
-        .await?;
-
-    workspace_manager
-        .open_workspace(&input)
-        .await
-        .map_err(|err| TauriError(format!("Failed to open workspace: {}", err))) // TODO: improve error handling
-}
 
 #[tauri::command(async)]
 #[instrument(level = "trace", skip(app_manager), fields(window = window.label()))]
