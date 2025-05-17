5d8b1800e4ec97025e92c8e46fc31a3958c64110
brutusyhy <142066452+brutusyhy@users.noreply.github.com>
2025-05-17 15:01:53 +0300

    initial implementation of new `Storage`

--- crates/moss-storage/src/storage.rs
+++ crates/moss-storage/src/storage.rs
@@ -2,22 +2,24 @@ pub mod operations;
 pub mod table;
 
 use async_trait::async_trait;
-use moss_db::{DatabaseResult, ReDbClient, Transaction, primitives::AnyValue};
+use moss_db::primitives::AnyValue;
+use moss_db::{DatabaseClient, DatabaseResult, ReDbClient, Transaction};
+use serde_json::Value as JsonValue;
 use std::{any::TypeId, collections::HashMap, future::Future, path::Path, pin::Pin, sync::Arc};
 use table::Table;
 
 use crate::primitives::segkey::SegKeyBuf;
 
-// FIXME: Does this need to be an async trait?
 #[async_trait]
 pub trait Transactional {
     async fn begin_write(&self) -> DatabaseResult<Transaction>;
     async fn begin_read(&self) -> DatabaseResult<Transaction>;
 }
 
+#[async_trait]
 pub trait Storage: Transactional + Send + Sync {
-    // fn table(&self) -> DatabaseResult<Arc<dyn Table<SegKeyBuf, AnyValue>>>;
-    // fn dump(&self) -> DatabaseResult<HashMap<String, JsonValue>>;
+    async fn table(&self, id: &TypeId) -> DatabaseResult<Arc<dyn Table>>;
+    async fn dump(&self) -> DatabaseResult<HashMap<String, JsonValue>>;
 }
 
 #[async_trait]
@@ -39,7 +41,7 @@ impl StorageHandle {
         let mut client = ReDbClient::new(path.as_ref())?;
         let mut known_tables = HashMap::new();
         for table in tables {
-            client = client.with_table(&table.definition())?;
+            client = client.with_table(table.definition())?;
             known_tables.insert(table.type_id(), Arc::from(table) as Arc<dyn Table>);
         }
 
@@ -47,5 +49,106 @@ impl StorageHandle {
             client,
             tables: known_tables,
         })
+    }
+}
+
+#[async_trait]
+impl Transactional for StorageHandle {
+    async fn begin_write(&self) -> DatabaseResult<Transaction> {
+        self.client.begin_write()
+    }
+
+    async fn begin_read(&self) -> DatabaseResult<Transaction> {
+        self.client.begin_read()
+    }
+}
+
+#[async_trait]
+impl Storage for StorageHandle {
+    async fn table(&self, id: &TypeId) -> DatabaseResult<Arc<dyn Table>> {
+        Ok(self.tables.get(id).unwrap().clone())
+    }
+    async fn dump(&self) -> DatabaseResult<HashMap<String, JsonValue>> {
+        // FIXME: Error propagation seems to make pipelining very tricky
+
+        let read_txn = self.begin_read().await?;
+        let mut result = HashMap::new();
+        for table in self.tables.values() {
+            for (k, v) in table.definition().scan(&read_txn)? {
+                result.insert(k.to_string(), serde_json::from_slice(v.as_bytes())?);
+            }
+        }
+
+        Ok(result)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use crate::primitives::segkey::SegKey;
+    use moss_db::bincode_table::BincodeTable;
+    use moss_db::primitives::AnyValue;
+    use moss_testutils::random_name::random_string;
+    use std::ops::Deref;
+
+    struct Table1 {
+        table: BincodeTable<'static, SegKeyBuf, AnyValue>,
+    }
+    impl Table1 {
+        pub fn new() -> Self {
+            Self {
+                table: BincodeTable::new("table1"),
+            }
+        }
+    }
+    impl Table for Table1 {
+        fn definition(&self) -> &BincodeTable<SegKeyBuf, AnyValue> {
+            &self.table
+        }
+    }
+
+    struct TestStore {
+        handle: Arc<dyn Storage>,
+    }
+
+    impl TestStore {
+        pub fn new(path: &Path) -> Self {
+            Self {
+                handle: Arc::new(StorageHandle::new(path, vec![Table1::new()]).unwrap()),
+            }
+        }
+        pub async fn table1(&self) -> DatabaseResult<Arc<dyn Table>> {
+            self.handle.table(&TypeId::of::<Table1>()).await
+        }
+    }
+
+    impl Deref for TestStore {
+        type Target = Arc<dyn Storage>;
+
+        fn deref(&self) -> &Self::Target {
+            &self.handle
+        }
+    }
+
+    #[tokio::test]
+    async fn test_storage() {
+        let store = TestStore::new(&Path::new("tests").join(format!("{}.db", random_string(10))));
+        let table1 = store.table1().await.unwrap();
+
+        let mut write_txn = store.begin_write().await.unwrap();
+
+        table1
+            .definition()
+            .insert(
+                &mut write_txn,
+                SegKey::new("table1").join("key1"),
+                &AnyValue::new(serde_json::to_vec("value1").unwrap()),
+            )
+            .unwrap();
+
+        write_txn.commit().unwrap();
+
+        dbg!(store.dump().await.unwrap());
     }
 }
--- crates/moss-storage/src/storage/table.rs
+++ crates/moss-storage/src/storage/table.rs
@@ -1,10 +1,10 @@
+use crate::primitives::segkey::SegKeyBuf;
 use moss_db::{bincode_table::BincodeTable, primitives::AnyValue};
 use std::any::TypeId;
+use std::ops::Deref;
 
-use crate::primitives::segkey::SegKeyBuf;
-
-pub trait Table {
-    fn definition(&self) -> BincodeTable<SegKeyBuf, AnyValue>;
+pub trait Table: Send + Sync {
+    fn definition(&self) -> &BincodeTable<SegKeyBuf, AnyValue>;
     fn type_id(&self) -> TypeId
     where
         Self: 'static,
