eeacbefa674c59ec34efce372b897f4bed1ac45f
EnthusedFear <enthceph@gmail.com>
2025-05-18 15:26:09 +0300

    fix long names on welcome page

--- view/desktop/src/pages/WelcomePage/WelcomePageLink.tsx
+++ view/desktop/src/pages/WelcomePage/WelcomePageLink.tsx
@@ -7,8 +7,9 @@ export interface WelcomePageLinkProps extends React.AnchorHTMLAttributes<HTMLAnc
 
 export const WelcomePageLink = ({ label, withIcon, ...props }: WelcomePageLinkProps) => {
   return (
-    <a className="inline-flex cursor-pointer items-center text-(--moss-primary)" {...props}>
-      <span className="hover:underline">{label}</span> {withIcon && <Icon icon="ExternalLinkActive" />}
+    <a className="flex w-full min-w-0 cursor-pointer items-center text-(--moss-primary)" {...props}>
+      <span className="truncate hover:underline">{label}</span>
+      {withIcon && <Icon icon="ExternalLinkActive" />}
     </a>
   );
 };
