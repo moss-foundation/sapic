1a6aec9ae5d2c491eed6bba504998ba46d79e52a
g10z3r <20g10z3r@gmail.com>
2025-05-27 18:56:39 +0200

    Refactor schema generation in moss-settings to use a dynamic function, improving maintainability and clarity

--- crates/moss-settings/index.ts
+++ crates/moss-settings/index.ts
@@ -7,38 +7,44 @@ const enum Level {
   Workspace = 3,
 }
 
-const schema = z.object({
-  "window.defaultWidth": z
-    .number()
-    .min(800)
-    .max(3840)
-    .default(800)
-    .optional()
-    .meta({
-      level: Level.Application,
-    })
-    .describe("The width of the application window in pixels."),
-
-  "window.defaultHeight": z
-    .number()
-    .min(600)
-    .max(2160)
-    .default(600)
-    .optional()
-    .meta({
-      level: Level.Application,
-    })
-    .describe("The height of the application window in pixels."),
-});
+const schema = (obj: Record<string, z.ZodSchema>): string => {
+  return JSON.stringify(z.toJSONSchema(z.object(obj)), null, 2);
+};
 
 (async () => {
   try {
     mkdirSync("./gen/schemas", { recursive: true });
 
     await Promise.all([
-      writeFileSync(`./gen/schemas/default.schema.json`, JSON.stringify(z.toJSONSchema(schema), null, 2), {
-        flag: "w",
-      }),
+      writeFileSync(
+        `./gen/schemas/default.schema.json`,
+        schema({
+          "window.defaultWidth": z
+            .number()
+            .min(800)
+            .max(3840)
+            .default(800)
+            .optional()
+            .meta({
+              level: Level.Application,
+            })
+            .describe("The width of the application window in pixels."),
+
+          "window.defaultHeight": z
+            .number()
+            .min(600)
+            .max(2160)
+            .default(600)
+            .optional()
+            .meta({
+              level: Level.Application,
+            })
+            .describe("The height of the application window in pixels."),
+        }),
+        {
+          flag: "w",
+        }
+      ),
     ]);
   } catch (err) {
     console.error("Error generating themes:", err);
