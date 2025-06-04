7a33ee8602aaae6208216abd098052a89d83c6bf
brutusyhy <142066452+brutusyhy@users.noreply.github.com>
2025-06-04 18:17:03 +0300

    Update constants.rs

--- crates/moss-logging/src/constants.rs
+++ crates/moss-logging/src/constants.rs
@@ -4,4 +4,14 @@ pub const RESOURCE_LIT: &'static str = "resource";
 pub const APP_SCOPE: &'static str = "app";
 pub const SESSION_SCOPE: &'static str = "session";
 
+// ##################################################################
+// ###                                                            ###
+// ### !!! PLEASE UPDATE THE TYPESCRIPT CONSTANTS IN index.ts !!! ###
+// ###                                                            ###
+// ##################################################################
+
+// When adding/removing/modifying the constants below,
+// you must also update the corresponding TypeScript constants
+// in moss-logging/index.ts to match your changes.
+
 pub const LOGGING_SERVICE_CHANNEL: &'static str = "logging";
