852ea17bed6289237d1d1049c161cba30ae69a23
brutusyhy <142066452+brutusyhy@users.noreply.github.com>
2025-06-09 13:52:03 +0300

    Implement basic integration tests for `LoggingService`

--- crates/moss-logging/src/lib.rs
+++ crates/moss-logging/src/lib.rs
@@ -1,13 +1,9 @@
 pub mod api;
 mod constants;
-mod models;
+pub mod models;
+
 mod writers;
 
-use crate::{
-    constants::{APP_SCOPE, ID_LENGTH, SESSION_SCOPE},
-    models::types::LogEntryInfo,
-    writers::{rollinglog_writer::RollingLogWriter, taurilog_writer::TauriLogWriter},
-};
 use anyhow::Result;
 use moss_app::service::prelude::AppService;
 use moss_fs::FileSystem;
@@ -31,6 +27,12 @@ use tracing_subscriber::{
     prelude::*,
 };
 use uuid::Uuid;
+
+use crate::{
+    constants::{APP_SCOPE, ID_LENGTH, SESSION_SCOPE},
+    models::types::LogEntryInfo,
+    writers::{rollinglog_writer::RollingLogWriter, taurilog_writer::TauriLogWriter},
+};
 
 fn new_id() -> String {
     nanoid!(ID_LENGTH)
@@ -280,261 +282,17 @@ impl AppService for LoggingService {}
 
 #[cfg(test)]
 mod tests {
-    use super::*;
-    use crate::{
-        constants::LOGGING_SERVICE_CHANNEL,
-        models::{
-            operations::{DeleteLogInput, ListLogsInput},
-            types::LogLevel,
-        },
-    };
     use moss_fs::RealFileSystem;
     use moss_testutils::random_name::random_string;
-    use std::fs::create_dir_all;
+    use std::{fs::create_dir_all, sync::atomic::AtomicUsize, time::Duration};
     use tauri::{Listener, Manager};
     use tokio::fs::remove_dir_all;
-    use tracing::instrument;
 
-    #[instrument(level = "trace", skip_all)]
-    async fn test_create_collection(path: &Path, name: &str, log_service: &LoggingService) {
-        let collection_path = path.join(name);
-
-        log_service.info(
-            LogScope::Session,
-            LogPayload {
-                resource: Some(collection_path.to_string_lossy().to_string()),
-                message: format!(
-                    "Created collection {} at {}",
-                    name,
-                    collection_path.to_string_lossy().to_string()
-                ),
-            },
-        );
-        log_service.info(
-            LogScope::App,
-            LogPayload {
-                resource: Some(collection_path.to_string_lossy().to_string()),
-                message: "Successfully created collection".to_string(),
-            },
-        )
-    }
-
-    #[instrument(level = "trace", skip_all)]
-    async fn test_create_request(collection_path: &Path, name: &str, log_service: &LoggingService) {
-        let request_path = collection_path.join(name);
-        log_service.info(
-            LogScope::Session,
-            LogPayload {
-                resource: Some(request_path.to_string_lossy().to_string()),
-                message: format!(
-                    "Created request {} at {}",
-                    name,
-                    request_path.to_string_lossy().to_string()
-                ),
-            },
-        );
-        log_service.info(
-            LogScope::App,
-            LogPayload {
-                resource: Some(request_path.to_string_lossy().to_string()),
-                message: "Successfully created request".to_string(),
-            },
-        )
-    }
-
-    #[instrument(level = "trace", skip_all)]
-    async fn something_terrible(log_service: &LoggingService) {
-        log_service.warn(
-            LogScope::App,
-            LogPayload {
-                resource: None,
-                message: "Something bad!".to_string(),
-            },
-        );
-        log_service.error(
-            LogScope::App,
-            LogPayload {
-                resource: None,
-                message: "Something terrible!".to_string(),
-            },
-        );
-    }
+    use super::*;
+    use crate::constants::LOGGING_SERVICE_CHANNEL;
 
     fn random_app_log_path() -> PathBuf {
         Path::new("tests").join("data").join(random_string(10))
-    }
-
-    #[tokio::test]
-    async fn test_delete_log() {
-        let test_app_log_path = random_app_log_path();
-        create_dir_all(&test_app_log_path).unwrap();
-
-        let fs = Arc::new(RealFileSystem::new());
-        let mock_app = tauri::test::mock_app();
-        let session_id = Uuid::new_v4();
-        let logging_service = LoggingService::new(
-            fs,
-            mock_app.app_handle().clone(),
-            &test_app_log_path,
-            &session_id,
-        )
-        .unwrap();
-
-        for _ in 0..15 {
-            logging_service.debug(
-                LogScope::App,
-                LogPayload {
-                    resource: None,
-                    message: "Test".to_string(),
-                },
-            );
-            logging_service.debug(
-                LogScope::Session,
-                LogPayload {
-                    resource: None,
-                    message: "Test".to_string(),
-                },
-            )
-        }
-
-        let old_logs = logging_service
-            .list_logs(&ListLogsInput {
-                dates: vec![],
-                levels: vec![],
-                resource: None,
-            })
-            .await
-            .unwrap()
-            .contents;
-
-        // Test deleting both from the file and from the queue
-
-        let first_entry = old_logs.first().unwrap().clone();
-        let first_id = first_entry.id;
-        dbg!(&first_id);
-        let first_timestamp = first_entry.timestamp;
-        logging_service
-            .delete_log(&DeleteLogInput {
-                timestamp: first_timestamp,
-                id: first_id.clone(),
-            })
-            .await
-            .unwrap();
-
-        let last_entry = old_logs.last().unwrap().clone();
-        let last_id = last_entry.id;
-        dbg!(&last_id);
-        let last_timestamp = last_entry.timestamp;
-        logging_service
-            .delete_log(&DeleteLogInput {
-                timestamp: last_timestamp,
-                id: last_id.clone(),
-            })
-            .await
-            .unwrap();
-
-        let updated_logs = logging_service
-            .list_logs(&ListLogsInput {
-                dates: vec![],
-                levels: vec![],
-                resource: None,
-            })
-            .await
-            .unwrap()
-            .contents;
-
-        // Wait for all writes to finish
-        tokio::time::sleep(tokio::time::Duration::from_millis(500)).await;
-        assert_eq!(old_logs.len() - updated_logs.len(), 2);
-        assert!(updated_logs.iter().find(|log| log.id == first_id).is_none());
-        assert!(updated_logs.iter().find(|log| log.id == last_id).is_none());
-        remove_dir_all(&test_app_log_path).await.unwrap();
-    }
-
-    #[tokio::test]
-    async fn test_list_logs() {
-        let test_app_log_path = random_app_log_path();
-        create_dir_all(&test_app_log_path).unwrap();
-
-        let fs = Arc::new(RealFileSystem::new());
-        let mock_app = tauri::test::mock_app();
-        let session_id = Uuid::new_v4();
-        let logging_service = LoggingService::new(
-            fs,
-            mock_app.app_handle().clone(),
-            &test_app_log_path,
-            &session_id,
-        )
-        .unwrap();
-
-        // Creating more logs than the buffer size to test reading from files
-        for _ in 0..10 {
-            logging_service.warn(
-                LogScope::App,
-                LogPayload {
-                    resource: None,
-                    message: "Test".to_string(),
-                },
-            );
-            logging_service.debug(
-                LogScope::App,
-                LogPayload {
-                    resource: Some("resource".to_string()),
-                    message: "".to_string(),
-                },
-            );
-            logging_service.debug(
-                LogScope::App,
-                LogPayload {
-                    resource: None,
-                    message: "".to_string(),
-                },
-            );
-        }
-
-        // Wait for all writes to finish
-        tokio::time::sleep(tokio::time::Duration::from_millis(500)).await;
-
-        let warn_entries = logging_service
-            .list_logs(&ListLogsInput {
-                dates: vec![],
-                levels: vec![LogLevel::WARN],
-                resource: None,
-            })
-            .await
-            .unwrap()
-            .contents;
-        assert_eq!(warn_entries.len(), 10);
-        assert!(warn_entries.iter().all(|log| log.level == LogLevel::WARN));
-
-        let resource_entries = logging_service
-            .list_logs(&ListLogsInput {
-                dates: vec![],
-                levels: vec![],
-                resource: Some("resource".to_string()),
-            })
-            .await
-            .unwrap()
-            .contents;
-        assert_eq!(resource_entries.len(), 10);
-        assert!(
-            resource_entries
-                .iter()
-                .all(|log| log.resource == Some("resource".to_string()))
-        );
-
-        let all_entries = logging_service
-            .list_logs(&ListLogsInput {
-                dates: vec![],
-                levels: vec![],
-                resource: None,
-            })
-            .await
-            .unwrap()
-            .contents;
-        assert_eq!(all_entries.len(), 30);
-
-        remove_dir_all(&test_app_log_path).await.unwrap();
     }
 
     #[tokio::test]
@@ -553,15 +311,25 @@ mod tests {
         )
         .unwrap();
 
-        mock_app.listen(LOGGING_SERVICE_CHANNEL, |event| {
-            println!("{}", event.payload())
-        });
+        let counter = Arc::new(AtomicUsize::new(0));
 
-        let collection_path = Path::new("").join("TestCollection");
+        {
+            let counter = counter.clone();
+            mock_app.listen(LOGGING_SERVICE_CHANNEL, move |_| {
+                counter.fetch_add(1, std::sync::atomic::Ordering::SeqCst);
+            });
+        }
 
-        test_create_collection(Path::new(""), "TestCollection", &logging_service).await;
-        test_create_request(&collection_path, "TestRequest", &logging_service).await;
-        something_terrible(&logging_service).await;
+        logging_service.debug(
+            LogScope::App,
+            LogPayload {
+                resource: None,
+                message: "".to_string(),
+            },
+        );
+
+        tokio::time::sleep(Duration::from_millis(100)).await;
+        assert_eq!(counter.load(std::sync::atomic::Ordering::SeqCst), 1);
 
         remove_dir_all(&test_app_log_path).await.unwrap()
     }
---
+++ crates/moss-logging/tests/loggingservice_list_logs.rs
@@ -0,0 +1,188 @@
+use crate::shared::set_up_logging_service;
+use moss_logging::{
+    LogPayload, LogScope,
+    models::{operations::ListLogsInput, types::LogLevel},
+};
+use std::fs::remove_dir_all;
+
+mod shared;
+
+// We can't test dates filter now since we can't generate logs with custom dates
+
+#[tokio::test]
+async fn test_list_logs_empty() {
+    let (logging_service, applog_path) = set_up_logging_service().await;
+
+    let list_logs_result = logging_service
+        .list_logs(&ListLogsInput {
+            dates: vec![],
+            levels: vec![],
+            resource: None,
+        })
+        .await;
+
+    assert!(list_logs_result.is_ok());
+    let logs = list_logs_result.unwrap().contents;
+    assert!(logs.is_empty());
+
+    remove_dir_all(applog_path).unwrap();
+}
+
+#[tokio::test]
+async fn test_list_logs_no_filter() {
+    let (logging_service, applog_path) = set_up_logging_service().await;
+
+    // Check that both app logs and session logs are returned
+    logging_service.debug(
+        LogScope::App,
+        LogPayload {
+            resource: None,
+            message: "".to_string(),
+        },
+    );
+    logging_service.debug(
+        LogScope::Session,
+        LogPayload {
+            resource: None,
+            message: "".to_string(),
+        },
+    );
+    logging_service.warn(
+        LogScope::App,
+        LogPayload {
+            resource: None,
+            message: "".to_string(),
+        },
+    );
+    logging_service.warn(
+        LogScope::Session,
+        LogPayload {
+            resource: None,
+            message: "".to_string(),
+        },
+    );
+
+    let list_logs_result = logging_service
+        .list_logs(&ListLogsInput {
+            dates: vec![],
+            levels: vec![],
+            resource: None,
+        })
+        .await;
+    assert!(list_logs_result.is_ok());
+    let logs = list_logs_result.unwrap().contents;
+
+    assert_eq!(logs.len(), 4);
+    remove_dir_all(applog_path).unwrap();
+}
+
+#[tokio::test]
+async fn test_list_logs_filter_by_levels() {
+    let (logging_service, applog_path) = set_up_logging_service().await;
+    logging_service.debug(
+        LogScope::App,
+        LogPayload {
+            resource: None,
+            message: "".to_string(),
+        },
+    );
+    logging_service.warn(
+        LogScope::App,
+        LogPayload {
+            resource: None,
+            message: "".to_string(),
+        },
+    );
+    logging_service.error(
+        LogScope::App,
+        LogPayload {
+            resource: None,
+            message: "".to_string(),
+        },
+    );
+
+    let debug_logs = logging_service
+        .list_logs(&ListLogsInput {
+            dates: vec![],
+            levels: vec![LogLevel::DEBUG],
+            resource: None,
+        })
+        .await
+        .unwrap()
+        .contents;
+    assert_eq!(debug_logs.len(), 1);
+
+    let warn_logs = logging_service
+        .list_logs(&ListLogsInput {
+            dates: vec![],
+            levels: vec![LogLevel::WARN],
+            resource: None,
+        })
+        .await
+        .unwrap()
+        .contents;
+    assert_eq!(warn_logs.len(), 1);
+
+    let error_logs = logging_service
+        .list_logs(&ListLogsInput {
+            dates: vec![],
+            levels: vec![LogLevel::ERROR],
+            resource: None,
+        })
+        .await
+        .unwrap()
+        .contents;
+    assert_eq!(error_logs.len(), 1);
+
+    remove_dir_all(applog_path).unwrap();
+}
+
+#[tokio::test]
+async fn test_list_logs_filter_by_resource() {
+    let (logging_service, applog_path) = set_up_logging_service().await;
+    logging_service.debug(
+        LogScope::App,
+        LogPayload {
+            resource: None,
+            message: "".to_string(),
+        },
+    );
+    logging_service.debug(
+        LogScope::App,
+        LogPayload {
+            resource: Some("A".to_string()),
+            message: "".to_string(),
+        },
+    );
+    logging_service.debug(
+        LogScope::App,
+        LogPayload {
+            resource: Some("B".to_string()),
+            message: "".to_string(),
+        },
+    );
+
+    let a_logs = logging_service
+        .list_logs(&ListLogsInput {
+            dates: vec![],
+            levels: vec![],
+            resource: Some("A".to_string()),
+        })
+        .await
+        .unwrap()
+        .contents;
+    assert_eq!(a_logs.len(), 1);
+
+    let b_logs = logging_service
+        .list_logs(&ListLogsInput {
+            dates: vec![],
+            levels: vec![],
+            resource: Some("B".to_string()),
+        })
+        .await
+        .unwrap()
+        .contents;
+    assert_eq!(b_logs.len(), 1);
+
+    remove_dir_all(applog_path).unwrap();
+}
