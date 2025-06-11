845d9642aa8eda970b50dd24d4f6bcee1824bc36
EnthusedFear <enthceph@gmail.com>
2025-06-11 19:30:44 +0300

    got it working

---
+++ packages/config-eslint/index.d.ts
@@ -0,0 +1,5 @@
+import type { FlatConfig } from "@typescript-eslint/utils/ts-eslint";
+
+export declare const defineConfig: typeof import("typescript-eslint").config;
+
+export declare const defaultConfig: FlatConfig.ConfigArray;
--- view/desktop/eslint.config.js
+++ view/desktop/eslint.config.js
@@ -1,36 +1,43 @@
-import path, { dirname } from "node:path";
+import { dirname } from "node:path";
 import { fileURLToPath } from "node:url";
 
-import defaultConfig from "@repo/eslint-config/eslint.config.js";
+import { defaultConfig, defineConfig } from "@repo/eslint-config";
 
 const __dirname = dirname(fileURLToPath(import.meta.url));
 
-export default [
+export default defineConfig([
   ...defaultConfig,
   {
     ignores: ["**/moss-tabs/"],
   },
-  {
-    files: ["*.ts", "*.tsx"],
-    settings: {
-      "import/resolver": {
-        node: {
-          paths: ["src"],
-          extensions: [".js", ".jsx", ".ts", ".d.ts", ".tsx"],
-        },
-        typescript: {
-          project: "./tsconfig.json",
-        },
-        alias: {
-          map: [
-            ["@/assets", path.resolve(__dirname, "./src/assets")],
-            ["@/components", path.resolve(__dirname, "./src/components")],
-            ["@/hooks", path.resolve(__dirname, "./src/hooks")],
-            ["@/store", path.resolve(__dirname, "./src/store")],
-          ],
-          extensions: [".js", ".jsx", ".ts", ".d.ts", ".tsx"],
-        },
-      },
-    },
-  },
-];
+]);
+
+// export default [
+//   ...defaultConfig,
+//   {
+//     ignores: ["**/moss-tabs/"],
+//   },
+//   {
+//     files: ["*.ts", "*.tsx"],
+//     settings: {
+//       "import/resolver": {
+//         node: {
+//           paths: ["src"],
+//           extensions: [".js", ".jsx", ".ts", ".d.ts", ".tsx"],
+//         },
+//         typescript: {
+//           project: "./tsconfig.json",
+//         },
+//         alias: {
+//           map: [
+//             ["@/assets", path.resolve(__dirname, "./src/assets")],
+//             ["@/components", path.resolve(__dirname, "./src/components")],
+//             ["@/hooks", path.resolve(__dirname, "./src/hooks")],
+//             ["@/store", path.resolve(__dirname, "./src/store")],
+//           ],
+//           extensions: [".js", ".jsx", ".ts", ".d.ts", ".tsx"],
+//         },
+//       },
+//     },
+//   },
+// ];
