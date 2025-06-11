21be1d4be96647a1c86cbe4859f88b9aa8ec1555
EnthusedFear <enthceph@gmail.com>
2025-06-11 19:50:32 +0300

    got it working

--- packages/config-eslint/index.d.ts
+++
@@ -1,5 +0,0 @@
-import type { FlatConfig } from "@typescript-eslint/utils/ts-eslint";
-
-export declare const defineConfig: typeof import("typescript-eslint").config;
-
-export declare const defaultConfig: FlatConfig.ConfigArray;
