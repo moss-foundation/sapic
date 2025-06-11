f5dc439040076bc9494f328d941941f573269794
EnthusedFear <enthceph@gmail.com>
2025-06-11 20:20:13 +0300

    chore: update dependencies in ESLint configuration and package files; streamline Makefile for desktop installation

--- view/desktop/eslint.config.js
+++ view/desktop/eslint.config.js
@@ -11,33 +11,3 @@ export default defineConfig([
     ignores: ["**/moss-tabs/"],
   },
 ]);
-
-// export default [
-//   ...defaultConfig,
-//   {
-//     ignores: ["**/moss-tabs/"],
-//   },
-//   {
-//     files: ["*.ts", "*.tsx"],
-//     settings: {
-//       "import/resolver": {
-//         node: {
-//           paths: ["src"],
-//           extensions: [".js", ".jsx", ".ts", ".d.ts", ".tsx"],
-//         },
-//         typescript: {
-//           project: "./tsconfig.json",
-//         },
-//         alias: {
-//           map: [
-//             ["@/assets", path.resolve(__dirname, "./src/assets")],
-//             ["@/components", path.resolve(__dirname, "./src/components")],
-//             ["@/hooks", path.resolve(__dirname, "./src/hooks")],
-//             ["@/store", path.resolve(__dirname, "./src/store")],
-//           ],
-//           extensions: [".js", ".jsx", ".ts", ".d.ts", ".tsx"],
-//         },
-//       },
-//     },
-//   },
-// ];
