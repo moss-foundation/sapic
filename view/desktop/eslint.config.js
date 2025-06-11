afc6c8184b59d10acc5c9684c1502b161e606902
EnthusedFear <enthceph@gmail.com>
2025-06-11 20:26:08 +0300

    chore

--- view/desktop/eslint.config.js
+++ view/desktop/eslint.config.js
@@ -1,9 +1,4 @@
-import { dirname } from "node:path";
-import { fileURLToPath } from "node:url";
-
 import { defaultConfig, defineConfig } from "@repo/eslint-config";
-
-const __dirname = dirname(fileURLToPath(import.meta.url));
 
 export default defineConfig([
   ...defaultConfig,
