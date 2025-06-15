cefbe7c63751682f71d4ff8ba5724a360dbcd8ef
jevgenis33 <eugene230189@gmail.com>
2025-06-15 15:29:49 +0200

    style(SectionTitle): adjust width and layout for improved consistency in WorkspaceSettingsPage

--- view/desktop/src/pages/WorkspaceSettingsPage/SectionTitle.tsx
+++ view/desktop/src/pages/WorkspaceSettingsPage/SectionTitle.tsx
@@ -5,9 +5,9 @@ interface SectionTitleProps {
 
 export const SectionTitle = ({ children, className = "" }: SectionTitleProps) => {
   return (
-    <div className="mb-4 flex items-center gap-4">
+    <div className="mb-4 flex w-[36rem] items-center gap-4">
       <h3 className={`font-medium ${className}`}>{children}</h3>
-      <hr className="max-w-96 flex-1 border-gray-300" />
+      <hr className="flex-1 border-gray-300" />
     </div>
   );
 };
