87c18877cb94627e374836c51fabf7dfb3853830
g10z3r <20g10z3r@gmail.com>
2025-06-15 15:47:05 +0200

    Refactor API method signatures to accept references for input parameters in color theme, translations, locale, and collection management functions

--- crates/moss-app/src/api/set_color_theme.rs
+++ crates/moss-app/src/api/set_color_theme.rs
@@ -4,10 +4,10 @@ use tauri::Runtime as TauriRuntime;
 use crate::{app::App, models::operations::SetColorThemeInput};
 
 impl<R: TauriRuntime> App<R> {
-    pub async fn set_color_theme(&self, input: SetColorThemeInput) -> OperationResult<()> {
+    pub async fn set_color_theme(&self, input: &SetColorThemeInput) -> OperationResult<()> {
         // TODO: this implementation is not good enough, we need revisit it, and refactor it
         let mut theme_descriptor_lock = self.preferences.theme.write().await;
-        *theme_descriptor_lock = Some(input.theme_info);
+        *theme_descriptor_lock = Some(input.theme_info.clone());
 
         Ok(())
     }
--- crates/moss-app/src/api/set_locale.rs
+++ crates/moss-app/src/api/set_locale.rs
@@ -4,10 +4,10 @@ use tauri::Runtime as TauriRuntime;
 use crate::{app::App, models::operations::SetLocaleInput};
 
 impl<R: TauriRuntime> App<R> {
-    pub async fn set_locale(&self, input: SetLocaleInput) -> OperationResult<()> {
+    pub async fn set_locale(&self, input: &SetLocaleInput) -> OperationResult<()> {
         // TODO: this implementation is not good enough, we need revisit it, and refactor it
         let mut locale_lock = self.preferences.locale.write().await;
-        *locale_lock = Some(input.locale_info);
+        *locale_lock = Some(input.locale_info.clone());
 
         Ok(())
     }
