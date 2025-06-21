969ef04c226db8ef0074389ecf6a9a638870856d
brutusyhy <142066452+brutusyhy@users.noreply.github.com>
2025-06-21 12:41:42 +0300

    Update bindings

--- crates/moss-collection/index.ts
+++ crates/moss-collection/index.ts
@@ -1,12 +1,3 @@
-export * from "./bindings/events";
-export * from "./bindings/events.zod";
-export * from "./bindings/operations";
-export * from "./bindings/operations.zod";
-export * from "./bindings/primitives";
-export * from "./bindings/primitives.zod";
-export * from "./bindings/types";
-export * from "./bindings/types.zod";
-
 type RequestItemConfiguration = {
   url: string;
 };
@@ -16,3 +7,12 @@ export class RequestItem extends RequestItemConfiguration {
     super(configuration);
   }
 }
+
+export * from "./bindings/events";
+export * from "./bindings/events.zod";
+export * from "./bindings/operations";
+export * from "./bindings/operations.zod";
+export * from "./bindings/primitives";
+export * from "./bindings/primitives.zod";
+export * from "./bindings/types";
+export * from "./bindings/types.zod";
