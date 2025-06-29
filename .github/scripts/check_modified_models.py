c721a9914823d04165131b7122dd0eeb74edd95d
brutusyhy <142066452+brutusyhy@users.noreply.github.com>
2025-06-29 16:02:24 +0300

    Update check_modified_models.py

--- .github/scripts/check_modified_models.py
+++ .github/scripts/check_modified_models.py
@@ -25,4 +25,3 @@ if __name__ == "__main__":
     json_output = json.dumps(list(updated_models))
     with open(os.environ['GITHUB_OUTPUT'], 'a') as fh:
         fh.write(f'UPDATED_MODELS={json_output}\n')
-  
