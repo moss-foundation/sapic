fd79df9dae75b49ceef6854b4cade88df23c61e1
jevgenis33 <eugene230189@gmail.com>
2025-07-09 16:10:08 +0200

    Remove unused useDescribeWorkbenchState hook and its related export, and add new usePreviousWorkspace hook for tracking the previous workspace state.

--- view/desktop/src/hooks/workbench/index.ts
+++ view/desktop/src/hooks/workbench/index.ts
@@ -4,5 +4,4 @@ export * from "./useCloseWorkspace";
 export * from "./useCreateWorkspace";
 export * from "./useDeleteWorkspace";
 export * from "./useUpdateWorkspace";
-export * from "./useDescribeWorkbenchState";
 export * from "./useWorkspaceMapping";
--- view/desktop/src/hooks/workspace/index.ts
+++ view/desktop/src/hooks/workspace/index.ts
@@ -1,4 +1,5 @@
 export * from "./useDescribeWorkspaceState";
 export * from "./useActiveWorkspace";
+export * from "./usePreviousWorkspace";
 export * from "./useWorkspaceSidebarState";
 export * from "./useGlobalSidebarState";
