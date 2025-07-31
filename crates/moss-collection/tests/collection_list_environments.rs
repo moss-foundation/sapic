b8d6a9db327395ed2dd317af98fc8dc095c19052
brutusyhy <142066452+brutusyhy@users.noreply.github.com>
2025-07-31 17:25:12 +0200

    Remove debug stuff

--- crates/moss-collection/tests/collection_list_environments.rs
+++ crates/moss-collection/tests/collection_list_environments.rs
@@ -1,9 +1,2 @@
 // TODO: cant really test this, need to implement environments functionality first
 #![cfg(feature = "integration-tests")]
-
-// Trigger a CI workflow
-
-#[tokio::test]
-async fn test_warnings() {
-    let x = 42;
-}
