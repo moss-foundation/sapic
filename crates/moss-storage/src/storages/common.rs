8703704c2cecf7150f0de222ed160f4a55652dc4
brutusyhy <142066452+brutusyhy@users.noreply.github.com>
2025-08-05 21:30:51 +0200

    Implement DB operation traits on `VariableStore`

--- crates/moss-storage/src/storages/common.rs
+++ crates/moss-storage/src/storages/common.rs
@@ -1,5 +1,23 @@
 pub mod variable_store;
 
+use crate::{
+    primitives::segkey::SegKeyBuf,
+    storage::operations::{
+        GetItem, PutItem, RemoveItem, TransactionalGetItem, TransactionalPutItem,
+        TransactionalRemoveItem,
+    },
+};
 use moss_applib::context::AnyAsyncContext;
+use moss_db::primitives::AnyValue;
 
-pub trait VariableStore<Context: AnyAsyncContext>: Send + Sync {}
+pub trait VariableStore<Context: AnyAsyncContext>:
+    PutItem<Context, Key = SegKeyBuf, Entity = AnyValue>
+    + TransactionalPutItem<Context, Key = SegKeyBuf, Entity = AnyValue>
+    + GetItem<Context, Key = SegKeyBuf, Entity = AnyValue>
+    + TransactionalGetItem<Context, Key = SegKeyBuf, Entity = AnyValue>
+    + RemoveItem<Context, Key = SegKeyBuf, Entity = AnyValue>
+    + TransactionalRemoveItem<Context, Key = SegKeyBuf, Entity = AnyValue>
+    + Send
+    + Sync
+{
+}
