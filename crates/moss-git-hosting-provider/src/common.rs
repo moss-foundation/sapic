18964ada5018b1ff7fea46325d57497a38b11804
brutusyhy <142066452+brutusyhy@users.noreply.github.com>
2025-08-05 20:08:39 +0200

    Use `joinerror::Result` for `GitUrl::parse`

--- crates/moss-git-hosting-provider/src/common.rs
+++ crates/moss-git-hosting-provider/src/common.rs
@@ -1,6 +1,7 @@
 pub mod ssh_auth_agent;
 pub mod utils;
 
+use joinerror::Error;
 use moss_git::GitAuthAgent;
 
 pub trait SSHAuthAgent: GitAuthAgent {}
@@ -15,11 +16,11 @@ impl GitUrl {
     /// Parse a Git URL normalized by `moss_git::normalize_git_url`
     /// such as "github.com/user/repo"
 
-    pub fn parse(url: &str) -> anyhow::Result<GitUrl> {
+    pub fn parse(url: &str) -> joinerror::Result<GitUrl> {
         // FIXME: Handle more complex URL schemas
         let parts = url.split("/").collect::<Vec<_>>();
         if parts.len() != 3 {
-            return Err(anyhow::anyhow!("Unsupported Git Url: {}", url));
+            return Err(Error::new::<()>(format!("Unsupported Git URL: {}", url)));
         }
         Ok(GitUrl {
             domain: parts[0].to_string(),
