770748808b8d35ee1020baa923bd740cbc9faf35
EnthusedFear <enthceph@gmail.com>
2025-08-13 14:12:09 +0300

    style: Remove environment order display from WorkspacesListItemButton for a cleaner UI

--- view/desktop/src/components/WorkspacesList/WorkspacesListItem/WorkspacesListItemButton.tsx
+++ view/desktop/src/components/WorkspacesList/WorkspacesListItem/WorkspacesListItemButton.tsx
@@ -8,9 +8,8 @@ interface WorkspacesListItemButtonProps {
 export const WorkspacesListItemButton = ({ environment }: WorkspacesListItemButtonProps) => {
   return (
     <button className="z-10 flex cursor-pointer items-center gap-2 overflow-hidden">
-      <span className="text-xs text-gray-500 underline">{environment.order}</span>
       <Icon icon="Environment" />
-      <span className="truncate">{environment.name}</span> |<span className="truncate">{environment.id}</span>
+      <span className="truncate">{environment.name}</span>
     </button>
   );
 };
