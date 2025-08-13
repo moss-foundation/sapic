09ce6c3a7f14074a4295399ad275e2a8d584adfb
EnthusedFear <enthceph@gmail.com>
2025-08-13 21:29:47 +0300

    refactor: Enhance workspace management by adding environment query invalidation in useCloseWorkspace and useOpenWorkspace hooks

--- view/desktop/src/hooks/workbench/useCloseWorkspace.ts
+++ view/desktop/src/hooks/workbench/useCloseWorkspace.ts
@@ -5,12 +5,14 @@ import { useMutation, useQueryClient } from "@tanstack/react-query";
 import { USE_STREAMED_COLLECTION_ENTRIES_QUERY_KEY } from "..";
 import { USE_DESCRIBE_APP_STATE_QUERY_KEY } from "../appState/useDescribeAppState";
 import { USE_STREAMED_COLLECTIONS_QUERY_KEY } from "../collection";
+import { USE_STREAMED_ENVIRONMENTS_QUERY_KEY } from "../environment";
 import { USE_DESCRIBE_WORKSPACE_STATE_QUERY_KEY } from "../workspace";
 import { USE_LIST_WORKSPACES_QUERY_KEY } from "./useListWorkspaces";
 
 export const USE_CLOSE_WORKSPACE_QUERY_KEY = "closeWorkspace";
 
 const closeWorkspaceFn = async (workspaceId: string): Promise<CloseWorkspaceOutput> => {
+  console.log("onSuccess");
   const result = await invokeTauriIpc<CloseWorkspaceOutput>("close_workspace", {
     input: {
       id: workspaceId,
@@ -30,17 +32,20 @@ export const useCloseWorkspace = () => {
     mutationKey: [USE_CLOSE_WORKSPACE_QUERY_KEY],
     mutationFn: closeWorkspaceFn,
     onSuccess: () => {
-      // Invalidate other related queries
-      queryClient.invalidateQueries({ queryKey: [USE_LIST_WORKSPACES_QUERY_KEY] });
       queryClient.setQueryData([USE_LIST_WORKSPACES_QUERY_KEY], (old: ListWorkspacesOutput) => {
         return old.map((workspace) => ({ ...workspace, active: false }));
       });
+      queryClient.invalidateQueries({ queryKey: [USE_LIST_WORKSPACES_QUERY_KEY] });
+
+      // Invalidate other related queries
       queryClient.invalidateQueries({ queryKey: [USE_DESCRIBE_APP_STATE_QUERY_KEY] });
 
       // Remove ALL cached workspace state queries since no workspace is active
       queryClient.removeQueries({ queryKey: [USE_DESCRIBE_WORKSPACE_STATE_QUERY_KEY] });
       queryClient.removeQueries({ queryKey: [USE_STREAMED_COLLECTIONS_QUERY_KEY] });
       queryClient.removeQueries({ queryKey: [USE_STREAMED_COLLECTION_ENTRIES_QUERY_KEY] });
+      queryClient.removeQueries({ queryKey: [USE_STREAMED_ENVIRONMENTS_QUERY_KEY] });
+      queryClient.invalidateQueries({ queryKey: [USE_STREAMED_ENVIRONMENTS_QUERY_KEY] });
     },
   });
 };
