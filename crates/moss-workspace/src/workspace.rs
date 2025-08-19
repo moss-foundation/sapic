eb3903e365afb4b4280f8d8929fb20db1664fab7
brutusyhy <142066452+brutusyhy@users.noreply.github.com>
2025-08-19 19:32:11 +0200

    Fix warnings

--- crates/moss-workspace/src/workspace.rs
+++ crates/moss-workspace/src/workspace.rs
@@ -68,8 +68,8 @@ pub struct Workspace<R: AppRuntime> {
     pub(super) storage_service: Arc<StorageService<R>>,
 
     // TODO: Refine the management of git provider clients
-    pub(super) github_client: Arc<GitHubClient>,
-    pub(super) gitlab_client: Arc<GitLabClient>,
+    pub(super) _github_client: Arc<GitHubClient>,
+    pub(super) _gitlab_client: Arc<GitLabClient>,
 }
 
 impl<R: AppRuntime> AnyWorkspace<R> for Workspace<R> {
