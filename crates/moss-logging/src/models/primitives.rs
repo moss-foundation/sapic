40ad26440dcf99fb2def2b5589833fc5c4721d0a
brutusyhy <142066452+brutusyhy@users.noreply.github.com>
2025-08-21 19:32:30 +0200

    Use `generate_id_type` macro for all model ID types

--- crates/moss-logging/src/models/primitives.rs
+++ crates/moss-logging/src/models/primitives.rs
@@ -1,31 +1,6 @@
 use derive_more::Deref;
-use nanoid::nanoid;
+use moss_id_macro::generate_id_type;
 use serde::{Deserialize, Serialize};
-use std::{fmt::Display, sync::Arc};
+use std::fmt::Display;
 
-#[derive(Clone, Debug, PartialEq, Hash, Eq, Deref, Serialize, Deserialize)]
-#[serde(transparent)]
-pub struct LogEntryId(Arc<String>);
-impl LogEntryId {
-    pub fn new() -> Self {
-        Self(Arc::new(nanoid!(10)))
-    }
-}
-
-impl From<String> for LogEntryId {
-    fn from(s: String) -> Self {
-        Self(Arc::new(s))
-    }
-}
-
-impl AsRef<str> for LogEntryId {
-    fn as_ref(&self) -> &str {
-        self.0.as_ref()
-    }
-}
-
-impl Display for LogEntryId {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        write!(f, "{}", self.0)
-    }
-}
+generate_id_type!(LogEntryId);
