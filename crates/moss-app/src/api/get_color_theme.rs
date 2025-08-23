7b87ddcdc861c8138325079326aadbd51b4a10e2
g10z3r <20g10z3r@gmail.com>
2025-08-23 13:40:06 +0200

    refactor: Clean up unused code and improve theme retrieval logic
    - Removed commented-out code related to theme retrieval in `get_color_theme.rs`.
    - Deleted unused `Test.ts` and `api.rs` files from `moss-common`, streamlining the codebase.
    - Updated `lib.rs` to remove references to the deleted `api` module, enhancing clarity and maintainability.

--- crates/moss-app/src/api/get_color_theme.rs
+++ crates/moss-app/src/api/get_color_theme.rs
@@ -14,35 +14,5 @@ impl<R: AppRuntime> App<R> {
         let css_content = self.theme_service.read(&input.id).await?;
 
         Ok(GetColorThemeOutput { css_content })
-
-        // let themes = self.theme_service.themes().await?;
-
-        // if let Some(descriptor) = themes.get(&input.id) {
-        //     let css_content = {
-        //         let mut reader = self
-        //             .fs
-        //             .open_file(
-        //                 &self
-        //                     .theme_service
-        //                     .themes_dir
-        //                     .join(descriptor.source.clone()),
-        //             )
-        //             .await?;
-
-        //         let mut content = String::new();
-        //         reader
-        //             .read_to_string(&mut content)
-        //             .join_err::<()>("failed to read theme file")?;
-
-        //         content
-        //     };
-
-        //     Ok(GetColorThemeOutput { css_content })
-        // } else {
-        //     Err(Error::new::<()>(format!(
-        //         "theme with id `{}` not found",
-        //         input.id
-        //     )))
-        // }
     }
 }
