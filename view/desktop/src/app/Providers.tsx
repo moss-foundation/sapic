b5659bd1cd7382b024d88e7326850e9e343f80ad
EnthusedFear <enthceph@gmail.com>
2025-08-24 15:05:02 +0300

    refactor: consolidate theme initialization logic and remove unused dependencies in Providers and ThemeProvider

--- view/desktop/src/app/Providers.tsx
+++ view/desktop/src/app/Providers.tsx
@@ -4,8 +4,6 @@ import ErrorBoundary from "@/components/ErrorBoundary";
 import { useDescribeAppState } from "@/hooks/appState/useDescribeAppState";
 import { useOpenWorkspace } from "@/hooks/workbench/useOpenWorkspace";
 import { useActiveWorkspace } from "@/hooks/workspace/useActiveWorkspace";
-import { applyColorThemeFromCache } from "@/utils/applyTheme";
-import { useQueryClient } from "@tanstack/react-query";
 
 import LanguageProvider from "./LanguageProvider";
 import ThemeProvider from "./ThemeProvider";
@@ -23,23 +21,10 @@ const Providers = ({ children }: { children: ReactNode }) => {
 };
 
 const useInitializeAppState = () => {
-  const queryClient = useQueryClient();
-
   const { data } = useDescribeAppState();
   const { mutate: openWorkspace } = useOpenWorkspace();
   const { activeWorkspace } = useActiveWorkspace();
   const hasTriedRestoration = useRef(false);
-
-  // Initialize app theme and language
-  useEffect(() => {
-    if (data) {
-      const theme = data.preferences?.theme ?? data.defaults.theme;
-
-      document.querySelector("html")?.setAttribute("data-theme", theme.mode);
-
-      applyColorThemeFromCache(theme.identifier, queryClient);
-    }
-  }, [data, queryClient]);
 
   // Restore previous workspace if available
   useEffect(() => {
--- view/desktop/src/app/ThemeProvider.tsx
+++ view/desktop/src/app/ThemeProvider.tsx
@@ -1,6 +1,7 @@
 import { useEffect } from "react";
 
-import { USE_DESCRIBE_APP_STATE_QUERY_KEY } from "@/hooks/appState/useDescribeAppState";
+import { useGetColorTheme } from "@/hooks";
+import { USE_DESCRIBE_APP_STATE_QUERY_KEY, useDescribeAppState } from "@/hooks/appState/useDescribeAppState";
 import { applyColorThemeFromCache } from "@/utils/applyTheme";
 import { ColorThemeInfo } from "@repo/moss-app";
 import { useQueryClient } from "@tanstack/react-query";
@@ -8,6 +9,20 @@ import { listen, UnlistenFn } from "@tauri-apps/api/event";
 
 const ThemeProvider = ({ children }: { children: React.ReactNode }) => {
   const queryClient = useQueryClient();
+
+  const { data } = useDescribeAppState();
+  const { data: colorTheme } = useGetColorTheme({ themeId: data?.preferences?.theme?.identifier ?? "" });
+
+  // Initialize app theme
+  useEffect(() => {
+    if (data) {
+      const theme = data.preferences?.theme ?? data.defaults.theme;
+
+      document.querySelector("html")?.setAttribute("data-theme", theme.mode);
+
+      applyColorThemeFromCache(theme.identifier, queryClient);
+    }
+  }, [data, queryClient]);
 
   useEffect(() => {
     let unlisten: UnlistenFn | undefined;
