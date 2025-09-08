8c796cf47e91ad006b26ec55b3e1feaad2e00b15
EnthusedFear <enthceph@gmail.com>
2025-09-08 21:18:50 +0300

    attempt to fix eslint no unresolved issue in ci

--- view/desktop/src/components/Modals/Collection/NewCollectionModal/NewCollectionModal.tsx
+++ view/desktop/src/components/Modals/Collection/NewCollectionModal/NewCollectionModal.tsx
@@ -13,7 +13,7 @@ import { ModalWrapperProps } from "../../types";
 import { Divider } from "./components/Divider";
 import { ModeRadioGroup } from "./components/ModeRadioGroup";
 import { CREATE_TAB, IMPORT_TAB } from "./constansts";
-import { CreateSection, FooterActions, Header, ImportSection } from "./sections";
+import { CreateSection, FooterActions, Header, ImportSection } from "./Sections";
 import { calculateIsSubmitDisabled } from "./utils";
 
 interface NewCollectionModalProps extends ModalWrapperProps {
--- view/desktop/src/components/Modals/Collection/NewCollectionModal/components/ModeRadioGroup.tsx
+++ view/desktop/src/components/Modals/Collection/NewCollectionModal/components/ModeRadioGroup.tsx
@@ -1,6 +1,6 @@
 import { RadioGroup } from "@/components";
 
-import { Subheader } from "../sections/Subheader";
+import { Subheader } from "../Sections/Subheader";
 
 interface ModeRadioGroupProps {
   mode: "Default" | "Custom";
