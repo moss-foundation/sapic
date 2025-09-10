41fa97437a78967259aed52ea3da40674fc41243
EnthusedFear <enthceph@gmail.com>
2025-09-10 13:19:56 +0300

    feat: Add MossToggle component and update dependencies

--- view/desktop/src/lib/ui/index.ts
+++ view/desktop/src/lib/ui/index.ts
@@ -3,6 +3,7 @@ export * as Checkbox from "./Checkbox";
 export * from "./Icon";
 export * as Menu from "./Menu";
 export * from "./Modal";
+export { MossToggle } from "./MossToggle";
 export * as RadioGroup from "./RadioGroup";
 export * from "./Resizable";
 export * from "./Scrollbar";
