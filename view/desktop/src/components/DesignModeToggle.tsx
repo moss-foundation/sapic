b60670c07f458df1d9889b401da684ed18076a3c
EnthusedFear <enthceph@gmail.com>
2025-09-17 18:54:01 +0300

    refactor: enhance DesignModeToggle layout and PlannedBadge component for improved styling and responsiveness

--- view/desktop/src/components/DesignModeToggle.tsx
+++ view/desktop/src/components/DesignModeToggle.tsx
@@ -22,11 +22,11 @@ export const DesignModeToggle = ({ className, disabled = true }: DesignModeToggl
   return (
     <div
       className={cn(
-        "background-(--moss-display-mode-bg) @container/mode-toggle flex w-full items-center rounded-sm border-1 border-(--moss-display-mode-border) p-px",
+        "background-(--moss-display-mode-bg) @container/mode-toggle grid w-full grid-cols-2 items-center rounded-sm border-1 border-(--moss-display-mode-border) p-px",
         className
       )}
     >
-      <div className={cn("grow overflow-hidden", { "cursor-not-allowed opacity-50": disabled })}>
+      <div className={cn("")}>
         <input
           type="radio"
           id="live"
@@ -39,17 +39,14 @@ export const DesignModeToggle = ({ className, disabled = true }: DesignModeToggl
         <label
           htmlFor="live"
           className={cn(
-            "flex min-w-0 cursor-pointer items-center justify-center rounded-sm px-1 py-px leading-5 text-(--moss-display-mode-text) transition-colors duration-300 peer-checked:bg-white peer-checked:text-(--moss-display-mode-text-selected)",
-            {
-              "cursor-not-allowed": disabled,
-            }
+            "flex min-w-0 cursor-pointer items-center justify-center rounded-sm px-1 py-px leading-5 text-(--moss-display-mode-text) transition-colors duration-300 peer-checked:bg-white peer-checked:text-(--moss-display-mode-text-selected)"
           )}
         >
           <span className="w-full truncate text-center">Live</span>
         </label>
       </div>
 
-      <div className={cn("grow overflow-hidden", { "cursor-not-allowed opacity-50": disabled })}>
+      <div className={cn("flex items-center justify-center gap-1 overflow-hidden px-2.5")}>
         <input
           type="radio"
           id="design"
@@ -64,14 +61,14 @@ export const DesignModeToggle = ({ className, disabled = true }: DesignModeToggl
           className={cn(
             "flex min-w-0 cursor-pointer items-center justify-center rounded-sm px-1 py-px leading-5 text-(--moss-display-mode-text) transition-colors duration-300 peer-checked:bg-white peer-checked:text-(--moss-display-mode-text-selected)",
             {
-              "cursor-not-allowed": disabled,
+              "cursor-not-allowed opacity-50": disabled,
             }
           )}
         >
           <span className="w-full truncate text-center">Design</span>
         </label>
+        <PlannedBadge variant="outlined" className="hidden @min-[260px]/mode-toggle:block" />
       </div>
-      <PlannedBadge />
     </div>
   );
 };
