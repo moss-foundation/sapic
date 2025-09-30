546938fb74b6f69ac314604da807be1f1d9e400b
EnthusedFear <enthceph@gmail.com>
2025-09-30 13:16:12 +0300

    temporarily remove simbplebar and replace with css

--- view/desktop/src/components/PageView/PageView.tsx
+++ view/desktop/src/components/PageView/PageView.tsx
@@ -1,11 +1,12 @@
+import { Scrollbar } from "@/lib/ui";
 import { cn } from "@/utils";
 
 import { PageViewProps } from "./types";
 
 export const PageView = ({ children, className }: PageViewProps) => {
   return (
-    <div className={cn("background-(--moss-primary-background) relative flex h-full flex-col", className)}>
+    <Scrollbar className={cn("background-(--moss-primary-background) relative flex h-full flex-col", className)}>
       {children}
-    </div>
+    </Scrollbar>
   );
 };
--- view/desktop/src/lib/ui/Scrollbar.tsx
+++ view/desktop/src/lib/ui/Scrollbar.tsx
@@ -3,10 +3,11 @@ import SimpleBar, { type Props as SimpleBarProps } from "simplebar-react";
 import { cn } from "@/utils";
 
 export const Scrollbar = ({ children, className, classNames, ...props }: SimpleBarProps) => {
+  return <div className={cn("h-full w-full overflow-auto", className)}>{children}</div>;
   return (
     <SimpleBar
       classNames={{
-        contentEl: "h-full w-full",
+        contentEl: "w-full",
         ...classNames,
       }}
       className={cn("h-full w-full", className)}
