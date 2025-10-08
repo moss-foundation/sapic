80757feeebf715ee66c45b2ee95b18e44198f3d6
EnthusedFear <enthceph@gmail.com>
2025-10-08 19:35:50 +0300

    changed icons size in project tree to 18px

--- view/desktop/src/components/EntryIcon.tsx
+++ view/desktop/src/components/EntryIcon.tsx
@@ -12,18 +12,20 @@ interface EntryIconProps {
 const defaultProtocolClassName = "text-xs right-0 uppercase absolute top-1/2 -translate-y-1/2   " as const;
 
 export const EntryIcon = ({ entry, className }: EntryIconProps) => {
-  if (!entry) return <div className={cn("size-4 shrink-0 opacity-0", className)} />;
+  if (!entry) return <div className={cn("size-[18px] shrink-0 opacity-0", className)} />;
 
   if (entry.kind === "Dir") {
-    return <Icon icon="Folder" className={className} />;
+    return <Icon icon="Folder" className={cn("size-[18px]", className)} />;
   }
   if (entry.kind === "Item") {
-    return <Icon icon="Http" className={className} />;
+    return <Icon icon="Http" className={cn("size-[18px]", className)} />;
   }
 
   return (
-    <div className="relative size-4">
-      <span className={cn(defaultProtocolClassName, "text-(--moss-gray-4)", className)}>{entry.protocol}</span>
+    <div className="relative">
+      <span className={cn(defaultProtocolClassName, "size-[18px] text-(--moss-gray-4)", className)}>
+        {entry.protocol}
+      </span>
     </div>
   );
 };
