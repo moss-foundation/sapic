8ca48fd2679f4b999dbddd71e8ea1850d1596134
EnthusedFear <enthceph@gmail.com>
2025-10-08 17:30:53 +0300

    fix typo

--- view/desktop/src/parts/BottomPane/BottomPane.tsx
+++ view/desktop/src/parts/BottomPane/BottomPane.tsx
@@ -8,7 +8,7 @@ export const BottomPane = () => {
         <div className={`p-2 font-mono text-sm select-none hover:select-text`}>
           <div className="mb-2 font-semibold">Application Logs:</div>
 
-          {/* TODO removing this test data dont ofrget to remove testLogEntries.ts file  */}
+          {/* TODO removing this test data don't forget to remove testLogEntries.ts file  */}
           {testLogEntries.map((log, index) => (
             <div key={index} className="mb-1 flex">
               <span className="mr-2 text-(--moss-secondary-text)">{log.timestamp}</span>
