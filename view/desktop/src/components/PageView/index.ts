c1cf6649adaa683420b1297f26da357d3789ffbe
EnthusedFear <enthceph@gmail.com>
2025-10-17 14:32:19 +0300

    remove PageTabs component

--- view/desktop/src/components/PageView/index.ts
+++ view/desktop/src/components/PageView/index.ts
@@ -1,6 +1,5 @@
 export { PageContent } from "./PageContent";
 export { PageHeader } from "./PageHeader";
-export { PageTabs } from "./PageTabs";
 export { PageToolbar } from "./PageToolbar";
 export { PageView } from "./PageView";
-export type { PageContentProps, PageTabsProps, PageToolbarProps, PageViewProps } from "./types";
+export type { PageContentProps, PageToolbarProps, PageViewProps } from "./types";
