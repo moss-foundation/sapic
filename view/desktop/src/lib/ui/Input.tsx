d735b777ea5770464b4a51b09ce468cd1990e9a6
EnthusedFear <enthceph@gmail.com>
2025-10-24 18:56:47 +0300

    Add shortcut prop and update styles for Input

--- view/desktop/src/lib/ui/Input.tsx
+++ view/desktop/src/lib/ui/Input.tsx
@@ -2,13 +2,13 @@ import { cva } from "class-variance-authority";
 import React, { forwardRef, useRef } from "react";
 
 import { useInputResize } from "@/hooks/useInputResize";
-import { cn, mergeRefs } from "@/utils";
+import { mergeRefs } from "@/utils";
 
 import Icon, { Icons } from "./Icon";
 
 export interface InputProps extends Omit<React.InputHTMLAttributes<HTMLInputElement>, "size"> {
   iconLeft?: Icons;
-  iconRight?: Icons;
+  shortcut?: string;
   iconClassName?: string;
   inputFieldClassName?: string;
   fieldSizing?: "content" | "auto";
@@ -20,7 +20,7 @@ export interface InputProps extends Omit<React.InputHTMLAttributes<HTMLInputElem
 const inputWrapperStyles = cva(`
     flex items-center w-full gap-2
     border 
-    rounded-sm px-2 py-0 
+    rounded-md pl-2 pr-[5px] py-0 
 
     has-[input:focus-within]:outline-2 
     has-[input:focus-within]:outline-(--moss-accent)   
@@ -47,7 +47,11 @@ const inputWrapperStyles = cva(`
 );
 
 const inputStyles = cva(
-  `py-[7px] font-normal text-(--moss-controls-foreground) placeholder-(--moss-controls-placeholder)`
+  `text-(--moss-controls-foreground) placeholder-(--moss-controls-placeholder) h-auto w-full py-[5px] font-normal focus-visible:outline-none`
+);
+
+const shortcutStyles = cva(
+  `background-(--moss-gray-12) text-(--moss-gray-3) shrink-0 rounded-sm px-1 py-0.5 font-semibold`
 );
 
 export const Input = forwardRef<HTMLInputElement, InputProps>(
@@ -56,7 +60,7 @@ export const Input = forwardRef<HTMLInputElement, InputProps>(
       className,
       disabled = false,
       iconLeft,
-      iconRight,
+      shortcut,
       iconClassName,
       inputFieldClassName,
       fieldSizing = "auto",
@@ -71,17 +75,17 @@ export const Input = forwardRef<HTMLInputElement, InputProps>(
     useInputResize({ ref, enabled: fieldSizing === "content" });
 
     return (
-      <div className={cn(inputWrapperStyles({ disabled, contrast, intent }), className)}>
+      <div className={inputWrapperStyles({ disabled, contrast, intent, className })}>
         {iconLeft && <Icon icon={iconLeft} className={iconClassName} />}
 
         <input
           ref={mergeRefs([ref, forwardedRef])}
           disabled={disabled}
-          className={cn(inputStyles(), "h-auto w-full focus-visible:outline-none", inputFieldClassName)}
+          className={inputStyles({ className: inputFieldClassName })}
           {...props}
         />
 
-        {iconRight && <Icon icon={iconRight} className={iconClassName} />}
+        {shortcut && <span className={shortcutStyles()}>{shortcut}</span>}
       </div>
     );
   }
