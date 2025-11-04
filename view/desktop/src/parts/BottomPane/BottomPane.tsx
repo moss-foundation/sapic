f7aba7d83db56c2b017c8102f02728511547c75d
brutusyhy <142066452+brutusyhy@users.noreply.github.com>
2025-11-04 12:56:30 +0300

    Print actual logs in the BottomPane

--- view/desktop/src/parts/BottomPane/BottomPane.tsx
+++ view/desktop/src/parts/BottomPane/BottomPane.tsx
@@ -1,22 +1,34 @@
-import { testLogResources } from "@/assets/testLogEntries";
+import { useEffect, useState } from "react";
 import { Scrollbar } from "@/lib/ui";
+import { LogEntryInfo, ON_DID_APPEND_LOG_ENTRY_CHANNEL } from "@repo/moss-app";
+import { listen } from "@tauri-apps/api/event";
 
 export const BottomPane = () => {
+  const [logs, setLogs] = useState<LogEntryInfo[]>([]);
+
+  useEffect(() => {
+    const unlisten = listen(ON_DID_APPEND_LOG_ENTRY_CHANNEL, (event) => {
+      const log: LogEntryInfo = event.payload as LogEntryInfo;
+      setLogs([...logs, log]);
+    });
+    return () => {
+      unlisten.then((unlistenFn) => unlistenFn());
+    };
+  });
+
   return (
     <div className="background-(--moss-primary-background) h-full w-full">
       <Scrollbar className="h-full">
-        <div className={`p-2 font-mono text-sm select-none hover:select-text`}>
-          <div className="mb-2 font-semibold">Application Logs:</div>
-
-          {/* TODO removing this test data don't forget to remove testLogEntries.ts file  */}
-          {testLogResources.map((log, index) => (
+        <div className={`select-none p-2 font-mono text-sm hover:select-text`}>
+          <div className="mb-2 font-semibold">Application and Session Logs:</div>
+          {logs.map((log, index) => (
             <div key={index} className="mb-1 flex">
-              <span className="mr-2 text-(--moss-secondary-foreground)">{log.timestamp}</span>
+              <span className="text-(--moss-secondary-foreground) mr-2">{log.timestamp}</span>
               <span
                 className={`mr-2 min-w-16 font-medium ${
                   log.level === "ERROR"
                     ? "text-red-500"
-                    : log.level === "WARNING"
+                    : log.level === "WARN"
                       ? "text-amber-500"
                       : log.level === "DEBUG"
                         ? "text-blue-500"
@@ -25,7 +37,7 @@ export const BottomPane = () => {
               >
                 {log.level}
               </span>
-              <span className="mr-2 min-w-32 font-semibold">{log.service}:</span>
+              <span className="mr-2 min-w-32 font-semibold">{log.resource || "No Resource"}:</span>
               <span>{log.message}</span>
             </div>
           ))}
