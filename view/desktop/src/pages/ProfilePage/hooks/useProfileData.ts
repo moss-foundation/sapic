f7765a581ddc425f98ccd188a32942c997d5f193
Enthceph <enthceph@gmail.com>
2025-11-08 13:26:59 +0200

    chore: mark useProfileData as deprecated in favor of useDescribeApp

--- view/desktop/src/pages/ProfilePage/hooks/useProfileData.ts
+++ view/desktop/src/pages/ProfilePage/hooks/useProfileData.ts
@@ -1,5 +1,8 @@
 import { useDescribeApp } from "@/hooks/app/useDescribeApp";
 
+/**
+ * @deprecated just use useDescribeApp and destruct the profile from the appState
+ */
 export const useProfileData = () => {
   const { data: appState, isLoading, error, refetch } = useDescribeApp();
 
