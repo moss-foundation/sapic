aa3e0abe4bf60869ccdfd354fe39670b71496231
EnthusedFear <enthceph@gmail.com>
2025-11-14 17:09:18 +0200

    fix ci errors

--- view/desktop/src/hooks/workbench/layout/useGetLayout.tsx
+++ view/desktop/src/hooks/workbench/layout/useGetLayout.tsx
@@ -6,13 +6,8 @@ import { useQuery } from "@tanstack/react-query";
 export const USE_GET_LAYOUT_QUERY_KEY = "getLayout";
 
 const queryFn = async (activeWorkspaceId?: string): Promise<LayoutStateOutput> => {
-  if (!activeWorkspaceId) {
-    return defaultLayoutState;
-  }
-
-  const layout = (await layoutService.getLayout(activeWorkspaceId))?.value as unknown as LayoutStateOutput;
-
-  return layout;
+  if (!activeWorkspaceId) return defaultLayoutState;
+  return await layoutService.getLayout(activeWorkspaceId);
 };
 
 export const useGetLayout = () => {
