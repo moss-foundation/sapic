6f1ae37356e7c8d0d6a978b5a8a8bc82f3fce36f
EnthusedFear <enthceph@gmail.com>
2025-11-19 15:56:25 +0200

    restored updateConfiguration

--- view/desktop/src/lib/services/app/appConfigService.ts
+++ view/desktop/src/lib/services/app/appConfigService.ts
@@ -1,8 +1,18 @@
 import { invokeTauriServiceIpc } from "@/infra/ipc/tauri";
-import { DescribeAppOutput } from "@repo/window";
+import { DescribeAppOutput, UpdateConfigurationInput } from "@repo/window";
 
 export const appConfigService = {
   describeApp: async () => {
     return await invokeTauriServiceIpc<void, DescribeAppOutput>({ cmd: "describe_app" });
+  },
+  updateConfiguration: async (configuration: UpdateConfigurationInput) => {
+    return await invokeTauriServiceIpc<UpdateConfigurationInput, void>({
+      cmd: "update_configuration",
+      args: {
+        input: {
+          ...configuration,
+        },
+      },
+    });
   },
 };
