b600d5d53ade804533b3c5f373f561dad4cbae38
brutusyhy <142066452+brutusyhy@users.noreply.github.com>
2025-11-24 12:19:37 +0200

    Basic implementation for the parser and example

---
+++ plugins/url-parser/src/models/operations.rs
@@ -0,0 +1,27 @@
+// Parse Url
+
+use serde::Deserialize;
+use ts_rs::TS;
+
+// #[derive(Debug, Clone, Serialize, TS)]
+// #[ts(optional_fields)]
+// #[serde(rename_all = "camelCase")]
+// #[ts(export, export_to = "operations.ts")]
+//
+//
+// /// @category Operation
+// #[derive(Debug, Clone, Deserialize, TS)]
+// #[serde(rename_all = "camelCase")]
+// #[ts(export, export_to = "operations.ts")]
+// pub struct ParseUrlInput {
+//     pub url: String,
+// }
+//
+// /// @category Operation
+// #[derive(Debug, Clone, Serialize, TS)]
+// #[ts(optional_fields)]
+// #[serde(rename_all = "camelCase")]
+// #[ts(export, export_to = "operations.ts")]
+// pub struct ParseUrlOutput {
+//
+// }
