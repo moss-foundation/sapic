520ef8a40ca8120f03595966930b2b2771b3e748
g10z3r <20g10z3r@gmail.com>
2025-11-27 15:53:51 +0100

    Remove commented-out code for AccountKind, SessionKind

--- crates/moss-user/src/models/primitives.rs
+++ crates/moss-user/src/models/primitives.rs
@@ -10,36 +10,36 @@ ids!([ProfileId, AccountId]);
 #[cfg(any(test, feature = "integration-tests"))]
 impl ContextValue for AccountId {}
 
-/// @category Primitive
-#[derive(Debug, Deserialize, Serialize, PartialEq, Eq, Clone, TS)]
-#[serde(rename_all = "UPPERCASE")]
-#[ts(export, export_to = "primitives.ts")]
-pub enum AccountKind {
-    GitHub,
-    GitLab,
-}
+// /// @category Primitive
+// #[derive(Debug, Deserialize, Serialize, PartialEq, Eq, Clone, TS)]
+// #[serde(rename_all = "UPPERCASE")]
+// #[ts(export, export_to = "primitives.ts")]
+// pub enum AccountKind {
+//     GitHub,
+//     GitLab,
+// }
 
-#[derive(Debug, Deserialize, Serialize, PartialEq, Eq, Clone)]
-#[serde(rename_all = "UPPERCASE")]
-pub enum SessionKind {
-    OAuth,
-    PAT,
-}
+// #[derive(Debug, Deserialize, Serialize, PartialEq, Eq, Clone)]
+// #[serde(rename_all = "UPPERCASE")]
+// pub enum SessionKind {
+//     OAuth,
+//     PAT,
+// }
 
-impl From<SessionKind> for AccountAuthMethodKindForFrontend {
-    fn from(value: SessionKind) -> Self {
-        match value {
-            SessionKind::OAuth => AccountAuthMethodKindForFrontend::OAuth,
-            SessionKind::PAT => AccountAuthMethodKindForFrontend::PAT,
-        }
-    }
-}
+// impl From<SessionKind> for AccountAuthMethodKindForFrontend {
+//     fn from(value: SessionKind) -> Self {
+//         match value {
+//             SessionKind::OAuth => AccountAuthMethodKindForFrontend::OAuth,
+//             SessionKind::PAT => AccountAuthMethodKindForFrontend::PAT,
+//         }
+//     }
+// }
 
-#[derive(Debug, Deserialize, Serialize, PartialEq, Eq, Clone, TS)]
-#[serde(rename = "AccountAuthMethodKind")]
-#[serde(rename_all = "UPPERCASE")]
-#[ts(export, export_to = "primitives.ts")]
-pub enum AccountAuthMethodKindForFrontend {
-    OAuth,
-    PAT,
-}
+// #[derive(Debug, Deserialize, Serialize, PartialEq, Eq, Clone, TS)]
+// #[serde(rename = "AccountAuthMethodKind")]
+// #[serde(rename_all = "UPPERCASE")]
+// #[ts(export, export_to = "primitives.ts")]
+// pub enum AccountAuthMethodKindForFrontend {
+//     OAuth,
+//     PAT,
+// }
