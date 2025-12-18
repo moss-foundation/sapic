7486634bf37924677bd65c74a0a6ef8a5d85da6f
EnthusedFear <enthceph@gmail.com>
2025-12-18 15:46:30 +0200

    Refactor DefaultView component into its own file

--- view/desktop/src/workbench/ui/parts/TabbedPane/TabbedPaneComponents.tsx
+++ view/desktop/src/workbench/ui/parts/TabbedPane/TabbedPaneComponents.tsx
@@ -1,6 +1,3 @@
-import { cn } from "@/utils";
-import { useTabbedPaneStore } from "@/workbench/store/tabbedPane";
-import { PageContent, PageHeader, PageView } from "@/workbench/ui/components";
 import {
   AccountsView,
   AccountsViewProps,
@@ -21,36 +18,13 @@ import {
   WorkspaceSettingsView,
   WorkspaceSettingsViewProps,
 } from "@/workbench/views";
+import { DefaultView } from "@/workbench/views/DefaultView/DefaultView";
 
-import Metadata from "./DebugComponents/Metadata";
 import { DefaultViewProps } from "./types";
 
 //NOTE: every component has to have a props type that extends DefaultViewProps for the addOrFocusPanel function's type checking to work correctly
 export const tabbedPaneComponents = {
-  DefaultView: (props: DefaultViewProps) => {
-    const { showDebugPanels } = useTabbedPaneStore();
-
-    return (
-      <PageView>
-        <PageHeader icon="Placeholder" {...props} />
-        <PageContent className={cn("relative", showDebugPanels && "border-2 border-dashed border-orange-500")}>
-          <span className="pointer-events-none absolute left-1/2 top-1/2 flex -translate-x-1/2 -translate-y-1/2 transform flex-col opacity-50">
-            <span className="text-[42px] leading-[42px]">Default Page</span>
-            <span className="text-sm leading-3">This is a placeholder default page</span>
-          </span>
-
-          {showDebugPanels && (
-            <Metadata
-              onClick={() => {
-                props.api.setRenderer(props.api.renderer === "always" ? "onlyWhenVisible" : "always");
-              }}
-              api={props.api}
-            />
-          )}
-        </PageContent>
-      </PageView>
-    );
-  },
+  DefaultView: (props: DefaultViewProps) => <DefaultView {...props} />,
   EndpointView: (props: EndpointViewProps) => <EndpointView {...props} />,
   ProjectSettingsView: (props: ProjectSettingsViewProps) => <ProjectSettingsView {...props} />,
   FolderSettingsView: (props: FolderSettingsViewProps) => <FolderSettingsView {...props} />,
