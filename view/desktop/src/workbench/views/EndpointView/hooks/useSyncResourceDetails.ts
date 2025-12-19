0781d447af9c6da16f9069f0c261c143e617b2d2
Enthceph <enthceph@gmail.com>
2025-12-19 12:02:28 +0200

    fix double insert to collection on linux

--- view/desktop/src/workbench/views/EndpointView/hooks/useSyncResourceDetails.ts
+++ view/desktop/src/workbench/views/EndpointView/hooks/useSyncResourceDetails.ts
@@ -1,9 +1,7 @@
-import { useEffect, useEffectEvent } from "react";
+import { useEffect } from "react";
 
 import { useDescribeProjectResource } from "@/adapters/tanstackQuery/resource";
 import { resourceDetailsCollection } from "@/db/resourceDetailsCollection";
-import { DescribeResourceOutput } from "@repo/moss-project";
-import { eq, useLiveQuery } from "@tanstack/react-db";
 
 interface useSyncResourceDetailsProps {
   resourceId: string;
@@ -13,85 +11,44 @@ interface useSyncResourceDetailsProps {
 export const useSyncResourceDetails = ({ resourceId, projectId }: useSyncResourceDetailsProps) => {
   const { data: backendResourceDetails } = useDescribeProjectResource({ projectId, resourceId });
 
-  const { data: localResourceDetails } = useLiveQuery((q) =>
-    q
-      .from({ collection: resourceDetailsCollection })
-      .where(({ collection }) => eq(collection.id, resourceId))
-      .findOne()
-  );
-
-  useEffect(() => {
-    if (!localResourceDetails && backendResourceDetails) {
-      const placeholderResourceDetails = {
-        ...backendResourceDetails,
-        //FIXME this is a temporary solution because the backend returns hardcoded value for the url
-        url:
-          backendResourceDetails.url === "Hardcoded Value" || backendResourceDetails.url === undefined
-            ? "{{baseUrl}}/docs/:docId/tables/:tableIdOrName/columns?sort={{sortValue}}&limit=2"
-            : backendResourceDetails.url,
-        description: undefined,
-        body: undefined,
-        //TODO: zod schema type should be updated.
-        // Without this zod will throw and error because the descriptions that return from the backend are null
-        // But in the schema we set them as optional and not as nullable
-        //Error message:
-        //SchemaValidationError: Insert validation failed:
-        // - Expected string, received null - path: pathParams,0,description
-        pathParams: backendResourceDetails.pathParams.map((param) => ({
-          ...param,
-          description: param.description ?? undefined,
-        })),
-        queryParams: backendResourceDetails.queryParams.map((param) => ({
-          ...param,
-          description: param.description ?? undefined,
-        })),
-      };
-
-      resourceDetailsCollection.insert({
-        id: resourceId,
-        ...placeholderResourceDetails,
-      });
-    }
-  }, [backendResourceDetails, resourceId, localResourceDetails]);
-
-  const updateLocalResourceDetails = useEffectEvent((backendResourceDetails: DescribeResourceOutput) => {
-    if (!localResourceDetails || !backendResourceDetails) return;
-
-    resourceDetailsCollection.update(resourceId, (draft) => {
-      if (!draft) return;
-
-      const placeholderResourceDetails = {
-        ...backendResourceDetails,
-        id: resourceId,
-        //FIXME this is a temporary solution because the backend returns hardcoded value for the url
-        url:
-          backendResourceDetails.url === "Hardcoded Value" || backendResourceDetails.url === undefined
-            ? "{{baseUrl}}/docs/:docId/tables/:tableIdOrName/columns?sort={{sortValue}}&limit=2"
-            : backendResourceDetails.url,
-        description: undefined,
-        body: undefined,
-        //TODO: zod schema type should be updated.
-        // Without this zod will throw and error because the descriptions that return from the backend are null
-        // But in the schema we set them as optional and not as nullable
-        //Error message:
-        //SchemaValidationError: Insert validation failed:
-        // - Expected string, received null - path: pathParams,0,description
-        pathParams: backendResourceDetails.pathParams.map((param) => ({
-          ...param,
-          description: param.description ?? undefined,
-        })),
-        queryParams: backendResourceDetails.queryParams.map((param) => ({
-          ...param,
-          description: param.description ?? undefined,
-        })),
-      };
-
-      Object.assign(draft, placeholderResourceDetails);
-    });
-  });
-
   useEffect(() => {
     if (!backendResourceDetails) return;
-    updateLocalResourceDetails(backendResourceDetails);
-  }, [backendResourceDetails]);
+
+    const placeholderResourceDetails = {
+      ...backendResourceDetails,
+      id: resourceId,
+      //FIXME this is a temporary solution because the backend returns hardcoded value for the url
+      url:
+        backendResourceDetails.url === "Hardcoded Value" || backendResourceDetails.url === undefined
+          ? "{{baseUrl}}/docs/:docId/tables/:tableIdOrName/columns?sort={{sortValue}}&limit=2"
+          : backendResourceDetails.url,
+      description: undefined,
+      body: undefined,
+      //TODO: zod schema type should be updated.
+      // Without this zod will throw and error because the descriptions that return from the backend are null
+      // But in the schema we set them as optional and not as nullable
+      //Error message:
+      //SchemaValidationError: Insert validation failed:
+      // - Expected string, received null - path: pathParams,0,description
+      pathParams: backendResourceDetails.pathParams.map((param) => ({
+        ...param,
+        description: param.description ?? undefined,
+      })),
+      queryParams: backendResourceDetails.queryParams.map((param) => ({
+        ...param,
+        description: param.description ?? undefined,
+      })),
+    };
+
+    const hasResourceDetails = resourceDetailsCollection.has(resourceId);
+
+    if (!hasResourceDetails) {
+      resourceDetailsCollection.insert(placeholderResourceDetails);
+    } else {
+      resourceDetailsCollection.update(resourceId, (draft) => {
+        if (!draft) return;
+        Object.assign(draft, placeholderResourceDetails);
+      });
+    }
+  }, [backendResourceDetails, resourceId]);
 };
