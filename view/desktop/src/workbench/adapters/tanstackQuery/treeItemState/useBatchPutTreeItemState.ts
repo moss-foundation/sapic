e8484005e4769d2c261d2375dbe8aefc96579eef
EnthusedFear <enthceph@gmail.com>
2026-02-02 18:30:01 +0200

    remove unnecessary call to backend

--- view/desktop/src/workbench/adapters/tanstackQuery/treeItemState/useBatchPutTreeItemState.ts
+++ view/desktop/src/workbench/adapters/tanstackQuery/treeItemState/useBatchPutTreeItemState.ts
@@ -2,7 +2,6 @@ import { treeItemStateService } from "@/workbench/domains/treeItemState/service"
 import { TreeItemState } from "@/workbench/domains/treeItemState/types";
 import { useMutation, useQueryClient } from "@tanstack/react-query";
 
-import { USE_BATCH_GET_TREE_ITEM_STATE_QUERY_KEY } from "./useBatchGetTreeItemState";
 import { USE_GET_TREE_ITEM_STATE_QUERY_KEY } from "./useGetTreeItemState";
 
 export const USE_BATCH_PUT_TREE_ITEM_STATE_MUTATION_KEY = "batchPutTreeItemState";
@@ -14,7 +13,6 @@ export const useBatchPutTreeItemState = () => {
     mutationKey: [USE_BATCH_PUT_TREE_ITEM_STATE_MUTATION_KEY],
     mutationFn: ({ treeItemStates, workspaceId }) => treeItemStateService.batchPut(treeItemStates, workspaceId),
     onSuccess: (_, { treeItemStates, workspaceId }) => {
-      queryClient.invalidateQueries({ queryKey: [USE_BATCH_GET_TREE_ITEM_STATE_QUERY_KEY] });
       treeItemStates.forEach((treeItemState) => {
         queryClient.setQueryData([USE_GET_TREE_ITEM_STATE_QUERY_KEY, treeItemState.id, workspaceId], treeItemState);
       });
