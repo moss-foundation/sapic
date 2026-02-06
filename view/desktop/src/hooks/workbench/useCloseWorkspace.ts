f974bd6ca56e18f6f054f6c0bc85974f87963c92
Enthceph <enthceph@gmail.com>
2026-02-06 22:10:39 +0200

    chore

--- view/desktop/src/hooks/workbench/useCloseWorkspace.ts
+++ view/desktop/src/hooks/workbench/useCloseWorkspace.ts
@@ -3,13 +3,9 @@ import { useMutation } from "@tanstack/react-query";
 
 export const USE_CLOSE_WORKSPACE_QUERY_KEY = "closeWorkspace";
 
-const closeWorkspaceFn = async (): Promise<void> => {
-  return await mainWorkspaceService.close();
-};
-
 export const useCloseWorkspace = () => {
   return useMutation<void, Error, string>({
     mutationKey: [USE_CLOSE_WORKSPACE_QUERY_KEY],
-    mutationFn: closeWorkspaceFn,
+    mutationFn: mainWorkspaceService.close,
   });
 };
