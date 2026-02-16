7e21a86f9c9568bd2843fb49b7dbbd3843cf6e34
Enthceph <enthceph@gmail.com>
2026-02-16 13:21:03 +0200

    feat: Add EnvironmentAddForm component

--- view/desktop/src/workbench/ui/components/EnvironmentsLists/WorkspaceEnvironmentsList/WorkspaceEnvironmentsListRoot.tsx
+++ view/desktop/src/workbench/ui/components/EnvironmentsLists/WorkspaceEnvironmentsList/WorkspaceEnvironmentsListRoot.tsx
@@ -1,33 +1,54 @@
 import { useRef } from "react";
 
+import { useCreateEnvironment } from "@/adapters";
 import { useGetWorkspaceEnvironments } from "@/db/environmentsSummaries/hooks/useGetWorkspaceEnvironments";
 import { useCurrentWorkspace } from "@/hooks";
 import { Tree } from "@/lib/ui/Tree";
 import { cn } from "@/utils";
+import { usePutEnvironmentItemState } from "@/workbench/adapters/tanstackQuery/environmentItemState/usePutEnvironmentItemState";
 import { useGetEnvironmentListItemState } from "@/workbench/adapters/tanstackQuery/environmentListItemState/useGetEnvironmentListItemState";
 
 import { WORKSPACE_ENVIRONMENTS_LIST_ID } from "../constants";
 import { useDropTargetWorkspaceEnvironmentList } from "../dnd/hooks/useDropTargetWorkspaceEnvironmentList";
+import { EnvironmentAddForm } from "../EnvironmentAddForm/EnvironmentAddForm";
 import { EnvironmentItem } from "../EnvironmentItem/EnvironmentItem";
 import { WorkspaceEnvironmentsListRootControls } from "./WorkspaceEnvironmentsListRootControls";
 
 export const WorkspaceEnvironmentsListRoot = () => {
   const { currentWorkspaceId } = useCurrentWorkspace();
-
-  const workspaceEnvironmentsListRef = useRef<HTMLUListElement>(null);
-
   const { workspaceEnvironments } = useGetWorkspaceEnvironments();
+  const { mutateAsync: createEnvironment } = useCreateEnvironment();
+  const { mutateAsync: putEnvironmentItemState } = usePutEnvironmentItemState();
 
   const { data: workspaceEnvironmentListItemState } = useGetEnvironmentListItemState(
     WORKSPACE_ENVIRONMENTS_LIST_ID,
     currentWorkspaceId
   );
+
+  const workspaceEnvironmentsListRef = useRef<HTMLUListElement>(null);
   const expanded = workspaceEnvironmentListItemState?.expanded ?? false;
 
   const { instruction } = useDropTargetWorkspaceEnvironmentList({
     refList: workspaceEnvironmentsListRef,
     workspaceEnvironments: workspaceEnvironments ?? [],
   });
+
+  const handleAddEnvironment = async (name: string) => {
+    const newEnvironmentOutput = await createEnvironment({
+      name,
+      color: undefined,
+      variables: [],
+    });
+    await putEnvironmentItemState({
+      environmentItemState: {
+        id: newEnvironmentOutput.id,
+        order: workspaceEnvironments.length,
+      },
+      workspaceId: currentWorkspaceId,
+    });
+  };
+
+  const restrictedNames = workspaceEnvironments?.map((environment) => environment.name) ?? [];
 
   return (
     <Tree.RootNode ref={workspaceEnvironmentsListRef} combineInstruction={instruction} className={cn("cursor-pointer")}>
@@ -45,6 +66,8 @@ export const WorkspaceEnvironmentsListRoot = () => {
           ))}
         </Tree.RootNodeChildren>
       )}
+
+      <EnvironmentAddForm onSubmit={handleAddEnvironment} restrictedNames={restrictedNames} />
     </Tree.RootNode>
   );
 };
